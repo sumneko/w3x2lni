f04beb251f7d2e2cf64348263d51c303c0f4fa9b
最萌小汐 <sunyijiusyj@126.com>
2016-10-11 10:36:27 +0800

    更新lni工具

--- src/lni.lua
+++ src/lni.lua
@@ -1,47 +1,156 @@
-local table_insert = table.insert
-local math_tointeger = math.tointeger
-local string_dump = string.dump
-local type = type
-local assert = assert
-local load = load
-local pairs = pairs
-local ipairs = ipairs
-local setmetatable = setmetatable
 local mt = {}
 local marco = {}
-local function split(str, p) local rt = {} str:gsub('[^'..p..']+', function (w) table_insert(rt, w) end) return rt end
-function mt:loader(code, ac, default)
+local function split(str, p) local rt = {} str:gsub('[^'..p..']+', function (w) table.insert(rt, w) end) return rt end
+local function trim(str) return str:gsub('^%s*(.-)%s*$', '%1') end
+local function dostring(file, ln, str, env) return assert(load(str, '=['..file..':'.. ln..']', 't', env))() end
+local function table_copy(tbl) local res = {} if tbl then for k, v in pairs(tbl) do res[k] = v end end return res end
+local function complie_computed(line, env)
+	local f, l = line:find('=', 1, true)
+	if not f then
+		return
+	end
+	local k, v = line:sub(1, f-1), line:sub(l+1)
+	v = v:gsub([[%'.-%']], function (s)
+		return [[_u:get ]] .. s
+	end):gsub('%[(.-)%]', function (s)
+		local x, y
+		local r = split(s, ',')
+		if #r == 0 then
+			return '[' .. s .. ']'
+		elseif #r == 1 then
+			x, y = s, s
+		elseif #r < env.max_level or #r == 2 then
+			x, y = r[1], r[#r]
+		else
+			return '({'..s..'})[_l*(max_level-1)+1]'
+		end
+		return '(('..x..') + (('..y..') - ('..x..')) * _l)'
+	end)
+	if env.max_level <= 1 then
+		return trim(k), 'local _l,_u=...;_l=0;return('..v..')'
+	end
+	return trim(k), 'local _l,_u=...;_l=(_l-1)/(max_level-1);return('..v..')'
+end
+local function format(n, formater)
+	if formater then
+		return ('%' .. formater):format(n)
+	elseif type(n) == 'number' then
+		return math.tointeger(n) or ('%.1f'):format(n)
+	end
+	return n
+end
+local fmter = {}
+function fmter:__tostring()
+	if not self.hero then
+		if self.fmt then
+			return self(('{:%s}[+{:%s}]'):format(self.fmt, self.fmt))
+		else
+			return self '{}[+{}]'
+		end
+	end
+	return format(self.func(self.level, self.hero), self.fmt)
+end
+function fmter:__call(formater)
+	local count = 0
+	local dummy = {}
+	local key
+	if not self.hero then
+		self.hero = {}
+		function self.hero:get(str)
+			key = str
+			return 0
+		end
+	end
+	local value_total = self.func(self.level, self.hero)
+	function dummy:get() return 0 end
+	local value_base = self.func(self.level, dummy)
+	function dummy:get() return 1 end
+	local value_factor = self.func(self.level, dummy) - value_base
+	return formater:gsub('%{(.-)%}', function(str)
+		count = count + 1
+		local fmt
+		local pos = str:find ':'
+		if pos then
+			fmt = str:sub(pos+1)
+			str = str:sub(1,pos-1)
+		end
+		local order = tonumber(str) or count
+		if order == 1 then
+			return format(value_base, fmt)
+		elseif order == 2 then
+			if key then
+				return ('%s*%s'):format(key, format(value_factor, fmt))
+			end
+			return format(value_total - value_base, fmt)
+		elseif order == 3 then
+			return format(value_factor, fmt)
+		else
+			return ''
+		end
+	end)
+end
+local function format_computed(str, hero, level, env, fmt)
+	local func = assert(load(str, '=(load)', 't', env))
+	return setmetatable({
+		func = func,
+		level = level,
+		hero = hero,
+		fmt = fmt,
+	}, fmter)
+end
+function mt:loader(code, file, ac, default, enum)
 	if code:sub(1, 3) == '\xEF\xBB\xBF' then code = code:sub(4) end
-	local function dostring(ln, str, env) return assert(load(str, '=line<' .. ln .. '>', 't', env))() end
-	local function trim(str) return str:gsub('^%s*(.-)%s*$', '%1') end
-	local function table_copy(tbl) local res = {} if tbl then for k, v in pairs(tbl) do res[k] = v end end return res end
-	local self = nil
+	local env = nil
 	local multi_table = nil
 	local multi_string = nil
+	file = file or '...'
 	ac = ac or {}
 	default = table_copy(default)
+	enum = table_copy(enum)
 	for ln, line in ipairs(split(code, '\n')) do
 		line = trim(line)
 		if multi_string then
 			multi_string = multi_string .. '\n' .. line
 			if line:sub(-2) == ']]' then
-				dostring(ln, multi_string, self)
+				dostring(file, ln, multi_string, env)
 				multi_string = nil
 			end
 		elseif multi_table then
 			multi_table = multi_table .. '\n' .. line
 			if line:sub(-1) == '}' then
-				dostring(ln, multi_table, self)
+				dostring(file, ln, multi_table, env)
 				multi_table = nil
 			end
 		else
 			if line:sub(1,1) == '[' then
 				local name = line:sub(3,-3)
 				if name == 'default' then
-					self = default
+					env = default
+				elseif name == 'enum' then
+					env = enum
 				else
-					self = setmetatable(table_copy(default), { __index = ac })
-					ac[name] = self
+					local computed = {}
+					local mt = {}
+					function mt:__index(k)
+						if k == 'computed' then
+							return computed
+						end
+						return enum[k]
+					end
+					function mt:__newindex(k, v)
+						if k == 'computed' then
+							for _, l in ipairs(split(v, '\n')) do
+								local k, v = complie_computed(trim(l), self)
+								if k then
+									computed[k] = v
+								end
+							end
+							return
+						end
+						rawset(self, k, v)
+					end
+					env = setmetatable(table_copy(default), mt)
+					ac[name] = env
 				end
 			elseif line:sub(1,2) == '--' then
 			elseif line:sub(-2) == '[[' then
@@ -49,20 +158,21 @@ function mt:loader(code, ac, default)
 			elseif line:sub(-1) == '{' then
 				multi_table = line
 			elseif line:sub(1,1) == [[']] then
-				dostring(ln, line:gsub([[^('.-')]], '_ENV[%1]'), self)
+				dostring(file, ln, line:gsub([[^('.-')]], '_ENV[%1]'), env)
 			elseif line:sub(1, 1) == [["]] then
-				dostring(ln, line:gsub([[^(".-")]], '_ENV[%1]'), self)
+				dostring(file, ln, line:gsub([[^(".-")]], '_ENV[%1]'), env)
 			elseif line:find([[^%d]]) ~= nil then
-				dostring(ln, line:gsub([[^(%d+)]], '_ENV[%1]'), self)
+				dostring(file, ln, line:gsub([[^(%d+)]], '_ENV[%1]'), env)
 			else
-				dostring(ln, line, self)
+				dostring(file, ln, line, env)
 			end
 		end
 	end
-	return ac, default
+	return ac, default, enum
 end
-function mt:unpacker(abil)
+function mt:normalize_then_unpack(abil)
 	local spell = {}
+	spell.computed = table_copy(abil.computed)
 	local max_level = abil.max_level
 	if not max_level then
 		max_level = 1
@@ -105,6 +215,47 @@ function mt:unpacker(abil)
 	end
 	return spell
 end
+function mt:normalize(abil)
+	-- todo
+end
+function mt:dostring(str, keyval, sep, hero, computed, level)
+	if not hero or type(hero) ~= 'table' then
+		hero = nil
+	else
+		function hero:get(name)
+			return self[name]
+		end
+	end
+	local fmt
+	local env = setmetatable({}, {__index = function(_, key)
+		if computed and computed[key] then
+			return format_computed(computed[key], hero, level, keyval, fmt)
+		end
+		if keyval[key] then
+			local value = keyval[key]
+			if type(value) == 'string' then
+				value = self:dostring(value, keyval, sep, hero, computed, level)
+			end
+			return value
+		end
+		return _G[key]
+	end})
+	return str:gsub('%' .. sep .. '(.-)%' .. sep, function(str)
+		local pos = str:find ':[%.%d]*%a$'
+		if pos then
+			fmt = str:sub(pos+1)
+			str = str:sub(1, pos-1)
+		else
+			fmt = nil
+		end
+		local value = assert(load('return (' .. str .. ')', '=(load)', 't', env))()
+		if type(value) == 'table' then
+			return tostring(value)
+		else
+			return format(value, fmt)
+		end
+	end)
+end
 function mt:dostringer(str, keyval, sep, hero)
 	if not hero or type(hero) ~= 'table' then
 		hero = {}
@@ -114,14 +265,14 @@ function mt:dostringer(str, keyval, sep, hero)
 	end
 	local env = setmetatable(keyval, {__index = _G})
 	local function setfenv(f)
-		return load(string_dump(f), '=(load)', nil, env)
+		return load(string.dump(f), '=(load)', nil, env)
 	end
 	local function dostring(str, env)
 		return str
 	end
 	local function format(n, env)
 		if type(n) == 'number' then
-			return math_tointeger(n) or ('%.1f'):format(n)
+			return math.tointeger(n) or ('%.1f'):format(n)
 		end
 		if type(n) == 'string' then
 			return dostring(n, env)
@@ -153,15 +304,23 @@ function mt:searcher(name, callback)
 end
 function mt:packager(name, loadfile)
 	local result = {}
-	local function package(path, default)
+	local ok = {}
+	local function package(path, default, enum)
+		ok[path] = true
 		local content = loadfile(path.. name .. '.ini')
 		if content then
-			result, default = self:loader(content, result, default)
+			result, default, enum = self:loader(content, path .. name .. '.ini', result, default, enum)
 		end
 		local config = loadfile(path .. '.iniconfig')
 		if config then
 			for _, dir in ipairs(split(config, '\n')) do
-				package(path .. dir:gsub('^%s', ''):gsub('%s$', '') .. '\\', default)
+				local name = dir:gsub('^%s', ''):gsub('%s$', '')
+				if name ~= '' then
+					local name = path .. name .. '\\'
+					if not ok[name] then
+						package(name, default, enum)
+					end
+				end
 			end
 		end
 	end
