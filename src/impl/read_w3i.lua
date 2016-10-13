2e512e68fe023908030995c4964f180f503df296
最萌小汐 <sunyijiusyj@126.com>
2016-10-13 14:56:04 +0800

    w3i转换为lni

--- src/impl/read_w3i.lua
+++ src/impl/read_w3i.lua
@@ -12,90 +12,91 @@ function mt:unpack(str)
 	return self:set_index(str:unpack(self.content, self.index))
 end
 
-function mt:add_head(chunk)
+function mt:add_head(data)
     -- 地图数据
-	chunk.file_ver,		--文件版本
-	chunk.map_ver,		--地图版本(保存次数)
-	chunk.editor_ver,	--编辑器版本
-	chunk.map_name,		--地图名称
-	chunk.author,		--作者名字
-	chunk.des,			--地图描述
-	chunk.player_rec	--推荐玩家
+	data.file_ver,		--文件版本
+	data.map_ver,		--地图版本(保存次数)
+	data.editor_ver,	--编辑器版本
+	data.map_name,		--地图名称
+	data.author,		--作者名字
+	data.des,			--地图描述
+	data.player_rec	--推荐玩家
     = self:unpack 'lllzzzz'
     
 	-- 镜头范围
-	chunk.camera_bound_1,
-	chunk.camera_bound_2,
-	chunk.camera_bound_3,
-	chunk.camera_bound_4,
-	chunk.camera_bound_5,
-	chunk.camera_bound_6,
-	chunk.camera_bound_7,
-	chunk.camera_bound_8
+	data.camera_bound_1,
+	data.camera_bound_2,
+	data.camera_bound_3,
+	data.camera_bound_4,
+	data.camera_bound_5,
+	data.camera_bound_6,
+	data.camera_bound_7,
+	data.camera_bound_8
     = self:unpack 'ffffffff'
     
     -- 镜头范围扩充
-	chunk.camera_complement_1,
-	chunk.camera_complement_2,
-	chunk.camera_complement_3,
-	chunk.camera_complement_4
+	data.camera_complement_1,
+	data.camera_complement_2,
+	data.camera_complement_3,
+	data.camera_complement_4
     = self:unpack 'llll'
 
     -- 地形属性
-	chunk.map_width,	        --地图宽度
-	chunk.map_height,	        --地图长度
-	chunk.map_flag,		        --地图标记,后面解读
-	chunk.map_main_ground_type	--地形类型
+	data.map_width,	        --地图宽度
+	data.map_height,	        --地图长度
+	data.map_flag,		        --地图标记,后面解读
+	data.map_main_ground_type	--地形类型
     = self:unpack 'lllc1'
 
     -- 载入图
-	chunk.loading_screen_id,	    --ID(-1表示导入载入图)
-	chunk.loading_screen_path,	    --路径
-	chunk.loading_screen_text,	    --文本
-	chunk.loading_screen_title,	    --标题
-	chunk.loading_screen_subtitle	--子标题
+	data.loading_screen_id,	    --ID(-1表示导入载入图)
+	data.loading_screen_path,	    --路径
+	data.loading_screen_text,	    --文本
+	data.loading_screen_title,	    --标题
+	data.loading_screen_subtitle	--子标题
     = self:unpack 'lzzzz'
 
     -- 使用的游戏数据设置
-	chunk.game_data_set = self:unpack 'l'
+	data.game_data_set = self:unpack 'l'
 
     -- 战役序幕
-	chunk.prologue_screen_path,		--路径
-	chunk.prologue_screen_text,		--文本
-	chunk.prologue_screen_title,	--标题
-	chunk.prologue_screen_subtitle	--子标题
+	data.prologue_screen_path,		--路径
+	data.prologue_screen_text,		--文本
+	data.prologue_screen_title,	--标题
+	data.prologue_screen_subtitle	--子标题
     = self:unpack 'zzzz'
 
     -- 迷雾
-	chunk.terrain_fog,	--类型
-	chunk.fog_start_z,	--开始z轴
-	chunk.fog_end_z,	--结束z轴
-	chunk.fog_density,	--密度
-	chunk.fog_red,		--红色
-	chunk.fog_green,	--绿色
-	chunk.fog_blue,		--蓝色
-	chunk.fog_alpha	    --透明
+	data.terrain_fog,	--类型
+	data.fog_start_z,	--开始z轴
+	data.fog_end_z,	--结束z轴
+	data.fog_density,	--密度
+	data.fog_red,		--红色
+	data.fog_green,	--绿色
+	data.fog_blue,		--蓝色
+	data.fog_alpha	    --透明
     = self:unpack 'lfffBBBB'
 	
     -- 环境
-	chunk.weather_id,           --天气
-	chunk.sound_environment,	--音效
-	chunk.light_environment	    --光照
+	data.weather_id,           --天气
+	data.sound_environment,	--音效
+	data.light_environment	    --光照
     = self:unpack 'c4zc1'
 
     -- 水
-	chunk.water_red,	--红色
-	chunk.water_green,	--绿色
-	chunk.water_blue,	--蓝色
-	chunk.water_alpha	--透明
+	data.water_red,	--红色
+	data.water_green,	--绿色
+	data.water_blue,	--蓝色
+	data.water_alpha	--透明
 	= self:unpack 'BBBB'
+end
 
-	-- 玩家
-	chunk.player_count = self:unpack 'l'
-	chunk.players = {}
-	for i = 1, chunk.player_count do
+function mt:add_player(data)
+	data.player_count = self:unpack 'l'
+	data.players = {}
+	for i = 1, data.player_count do
 		local player	= {}
-		chunk.players[i] = player
+		data.players[i] = player
 
 		player.id,
 		player.type,			--玩家类型(1玩家,2电脑,3野怪,4可营救)
@@ -106,7 +107,127 @@ function mt:add_head(chunk)
 		player.start_y,
 		player.ally_low_flag,	--低结盟优先权标记
 		player.ally_high_flag	--高结盟优先权标记
-		= self:unpack 'llllzffll'
+		= self:unpack 'llllzffLL'
+
+		player.ally_low_flag = player.ally_low_flag & ((1 << data.player_count) - 1)
+		player.ally_high_flag = player.ally_high_flag & ((1 << data.player_count) - 1)
+	end
+end
+
+function mt:add_force(data)
+	data.force_count = self:unpack 'l'
+	data.forces = {}
+	for i = 1, data.force_count do
+		local force	= {}
+		data.forces[i] = force
+
+		force.force_flag,	--队伍标记
+		force.player_flag,	--包含玩家
+		force.name
+		= self:unpack 'LLz'
+
+		force.player_flag = force.player_flag & ((1 << data.player_count) - 1)
+	end
+end
+
+function mt:add_upgrade(data)
+	data.upgrade_count = self:unpack 'l'
+	data.upgrades	= {}
+	for i = 1, data.upgrade_count do
+		local upgrade	= {}
+		table.insert(data.upgrades, upgrade)
+
+		upgrade.player_flag,	--包含玩家
+		upgrade.id,				--4位ID
+		upgrade.level,			--等级
+		upgrade.available		--可用性
+		= self:unpack 'lc4ll'
+	end
+end
+
+function mt:add_tech(data)
+	data.tech_count = self:unpack 'l'
+	data.techs	= {}
+	for i = 1, data.tech_count do
+		local tech	= {}
+		table.insert(data.techs, tech)
+
+		tech.player_flag,	--包含玩家
+		tech.id			--4位ID
+		= self:unpack 'lc4'
+	end
+end
+
+function mt:add_randomgroup(data)
+	data.group_count = self:unpack 'l'
+	data.groups = {}
+	for i = 1, data.group_count do
+		local group	= {}
+		table.insert(data.groups, group)
+
+		group.id,
+		group.name
+		= self:unpack 'lz'
+
+		--位置
+		group.position_count = self:unpack 'l'
+
+		group.positions	= {}
+		for i = 1, group.position_count do
+			group.positions[i] = self:unpack 'l'
+		end
+
+		--设置
+		group.line_count = self:unpack 'l'
+		
+		group.lines	= {}
+		for i = 1, group.line_count do
+			local line	= {}
+			table.insert(group.lines, line)
+
+			line.chance = self:unpack 'l'
+
+			--id列举
+			line.ids	= {}
+			for i = 1, group.position_count do
+				line.ids[i] = self:unpack 'c4'
+			end
+		end
+	end
+end
+
+function mt:add_randomitem(data)
+	data.random_item_count = self:unpack 'l'
+	data.random_items = {}
+
+	for i = 1, data.random_item_count do
+		local random_item = {}
+		table.insert(data.random_items, random_item)
+
+		random_item.id,
+		random_item.name
+		= self:unpack 'lz'
+
+		--设置
+		random_item.set_count = self:unpack 'l'
+		random_item.sets = {}
+		for i = 1, random_item.set_count do
+			local set = {}
+			table.insert(random_item.sets, set)
+
+			--物品
+			set.item_count = self:unpack 'l'
+			set.items = {}
+
+			for i = 1, set.item_count do
+				local item = {}
+				table.insert(set.items, item)
+
+				item.chance,
+				item.id
+				= self:unpack 'lc4'
+			end
+		end
 	end
 end
 
@@ -122,6 +243,12 @@ return function (self, content)
     tbl.index   = index
 
     tbl:add_head(data)
+	tbl:add_player(data)
+	tbl:add_force(data)
+	tbl:add_upgrade(data)
+	tbl:add_tech(data)
+	tbl:add_randomgroup(data)
+	tbl:add_randomitem(data)
     
     return data
 end
