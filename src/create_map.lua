fe3b03b0f60a57d09e2be0b83b2ace269fd1c341
最萌小汐 <sunyijiusyj@126.com>
2016-10-13 23:06:40 +0800

    打包地图

--- src/create_map.lua
+++ src/create_map.lua
@@ -1,3 +1,18 @@
+local stormlib = require 'stormlib'
+
+local table_insert = table.insert
+
+local function dir_scan(dir, callback)
+	for full_path in dir:list_directory() do
+		if fs.is_directory(full_path) then
+			-- 递归处理
+			dir_scan(full_path, callback)
+		else
+			callback(full_path)
+		end
+	end
+end
+
 local mt = {}
 mt.__index = mt
 
@@ -22,7 +37,87 @@ function mt:add_playercount()
     self:add('l', self.w3i.player_count)
 end
 
-return function (w3i)
+function mt:add_dir(dir)
+    table_insert(self.dirs, dir)
+end
+
+function mt:get_listfile(config)
+    local files = {}
+    local listfile = {}
+	local pack_ignore = {}
+	for _, name in ipairs(self.config['pack']['packignore']) do
+		pack_ignore[name:lower()] = true
+	end
+    
+    for _, dir in ipairs(self.dirs) do
+        local dir_len = #dir:string()
+        dir_scan(dir, function(path)
+            local name = path:string():sub(dir_len+2)
+            if not pack_ignore[name:lower()] then
+                listfile[#listfile+1] = name
+                files[name] = io.load(path)
+            end
+        end)
+    end
+	return listfile, files
+end
+
+function mt:import_files(map, listfile, files)
+	local clock = os.clock()
+	local success, failed = 0, 0
+	for i = 1, #listfile do
+		local name = listfile[i]
+		if map:save_file(name, files[name]) then
+			success = success + 1
+		else
+			failed = failed + 1
+			print('文件导入失败', name)
+		end
+		if os.clock() - clock >= 0.5 then
+			clock = os.clock()
+			print('正在导入', '成功:', success, '失败:', failed)
+		end
+	end
+	print('导入完毕', '成功:', success, '失败:', failed)
+end
+
+function mt:import_imp(map, listfile)
+	local imp_ignore = {}
+	for _, name in ipairs(self.config['pack']['impignore']) do
+		imp_ignore[name:lower()] = true
+	end
+
+	local imp = {}
+	for _, name in ipairs(listfile) do
+		if not imp_ignore[name:lower()] then
+			imp[#imp+1] = ('z'):pack(name)
+		end
+	end
+	table.insert(imp, 1, ('ll'):pack(1, #imp))
+	if not map:save_file('war3map.imp', table.concat(imp, '\r')) then
+		print('war3map.imp导入失败')
+	end
+end
+
+function mt:save(map_path)
+    local listfile, files = self:get_listfile(config)
+
+    io.save(map_path, table.concat(self.hexs))
+    
+    local map = stormlib.create(map_path, #listfile+8)
+	if not map then
+		print('地图创建失败,可能是文件被占用了')
+		return nil
+	end
+
+	self:import_files(map, listfile, files)
+	self:import_imp(map, listfile)
+
+    map:close()
+    return true
+end
+
+return function (config, w3i)
     if not w3i then
         w3i = {
             map_name = '只是另一张魔兽争霸III地图',
@@ -33,9 +128,12 @@ return function (w3i)
     local tbl = setmetatable({}, mt)
     tbl.hexs = {}
     tbl.w3i  = w3i
+    tbl.dirs = {}
+    tbl.config = config
+
     tbl:add_head()
     tbl:add_name()
     tbl:add_flag()
     tbl:add_playercount()
-    return table.concat(tbl.hexs)
+    return tbl
 end
--- src/make.lua
+++ src/make.lua
@@ -25,17 +25,6 @@ local function read_config()
 	end
 end
 
-local function dir_scan(dir, callback)
-	for full_path in dir:list_directory() do
-		if fs.is_directory(full_path) then
-			-- 递归处理
-			dir_scan(full_path, callback)
-		else
-			callback(full_path)
-		end
-	end
-end
-
 local count = 0
 local function get_temp_path()
 	count = count + 1
@@ -160,82 +149,6 @@ local function lni2w3x(input_path, output_dir)
 	end
 end
 
-local function get_listfile(input_path)
-	local pack_ignore = {}
-	for _, name in ipairs(config['pack']['packignore']) do
-		pack_ignore[name:lower()] = true
-	end
-
-	local parent_dir_len = #input_path:string()
-	local listfile = {}
-	dir_scan(input_path, function(path)
-		local name = path:string():sub(parent_dir_len+2)
-		if not pack_ignore[name:lower()] then
-			listfile[#listfile+1] = name
-		end
-	end)
-	return listfile
-end
-
-local function import_files(map, listfile, input_path)
-	local clock = os.clock()
-	local success, failed = 0, 0
-	for i = 1, #listfile do
-		local name = listfile[i]
-		local path = input_path / name
-		if map:add_file(name, path) then
-			success = success + 1
-		else
-			failed = failed + 1
-			print('文件导入失败', name)
-		end
-		if os.clock() - clock >= 0.5 then
-			clock = os.clock()
-			print('正在导入', '成功:', success, '失败:', failed)
-		end
-	end
-	print('导入完毕', '成功:', success, '失败:', failed)
-end
-
-local function import_imp(map, listfile)
-	local imp_ignore = {}
-	for _, name in ipairs(config['pack']['impignore']) do
-		imp_ignore[name:lower()] = true
-	end
-
-	local temp_path = root_dir / 'temp'
-	local imp = {}
-	for _, name in ipairs(listfile) do
-		if not imp_ignore[name:lower()] then
-			imp[#imp+1] = ('z'):pack(name)
-		end
-	end
-	table.insert(imp, 1, ('ll'):pack(1, #imp))
-
-	io.save(temp_path, table.concat(imp, '\r'))
-	if not map:add_file('war3map.imp', temp_path) then
-		print('war3map.imp导入失败')
-	end
-	fs.remove(temp_path)
-end
-
-local function pack(map_path, input_path)
-	local listfile = get_listfile(input_path)
-	fs.remove(map_path)
-
-	local w3i = w3x2txt:read_w3i(io.load(input_path / 'war3map.w3i'))
-	io.save(map_path, create_map(w3i))
-
-	local map = stormlib.create(map_path, #listfile+8)
-	if not map then
-		print('地图创建失败')
-		return
-	end
-	import_files(map, listfile, input_path)
-	import_imp(map, listfile)
-	map:close()
-end
-
 local function main()
 	if not arg[2] then
 		print('请将地图或文件夹拖动到bat中!')
@@ -247,10 +160,11 @@ local function main()
 	
 	local input_path = fs.path(uni.a2u(arg[2]))
 	if fs.is_directory(input_path) then
-		local map_name = 'new_' .. input_path:filename():string() .. '.w3x'
-		local map_path = input_path:parent_path() / map_name
-		lni2w3x(input_path, input_path)
-		pack(map_path, input_path)
+		local input_dir = input_path
+		local map_name = input_dir:filename():string() .. '.w3x'
+		local map_file = create_map(config, w3x2txt:read_w3i(io.load(input_dir / 'war3map.w3i')))
+		map_file:add_dir(input_dir)
+		map_file:save(input_dir:parent_path() / map_name)
 	else
 		local output_dir = root_dir / uni.a2u(fs.basename(input_path))
 		unpack(input_path, function(filename)
