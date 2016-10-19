d607f46d12e7237a3c5bab5666a2c71bcdd0f11b
最萌小汐 <sunyijiusyj@126.com>
2016-10-19 17:13:47 +0800

    加快sha1的速度

--- src/sha1.lua
+++ src/sha1.lua
@@ -8,77 +8,40 @@
 ---          stays intact.                    ---
 -------------------------------------------------
 
-local mod   = math.fmod
-local floor = math.floor
-local bit = {}
-
 ----------------------------------------
 
-local function cap(x)
-	return mod(x,4294967296)
+function bnot(x)
+	return 4294967295 ~ x
 end
 
 ----------------------------------------
 
-function bit.bnot(x)
-	return 4294967295-cap(x)
+function lshift(x,n)
+	return (x << n) & 4294967295
 end
 
 ----------------------------------------
 
-function bit.lshift(x,n)
-	return cap(cap(x)*2^n)
+function rshift(x,n)
+	return (x & 4294967295) >> n
 end
 
 ----------------------------------------
 
-function bit.rshift(x,n)
-	return floor(cap(x)/2^n)
+function band(x,y)
+	return (x & y) & 4294967295
 end
 
 ----------------------------------------
 
-function bit.band(x,y)
-	local z,i,j = 0,1
-	for j = 0,31 do
-		if (mod(x,2)==1 and mod(y,2)==1) then
-			z = z + i
-		end
-		x = bit.rshift(x,1)
-		y = bit.rshift(y,1)
-		i = i*2
-	end
-	return z
+function bor(x,y)
+	return (x | y) & 4294967295
 end
 
 ----------------------------------------
 
-function bit.bor(x,y)
-	local z,i,j = 0,1
-	for j = 0,31 do
-		if (mod(x,2)==1 or mod(y,2)==1) then
-			z = z + i
-		end
-		x = bit.rshift(x,1)
-		y = bit.rshift(y,1)
-		i = i*2
-	end
-	return z
-end
-
-----------------------------------------
-
-function bit.bxor(x,y)
-	local z,i,j = 0,1
-	for j = 0,31 do
-		if (mod(x,2)~=mod(y,2)) then
-			z = z + i
-		end
-		x = bit.rshift(x,1)
-		y = bit.rshift(y,1)
-		i = i*2
-	end
-	return z
+function bxor(x,y)
+	return (x ~ y) & 4294967295
 end
 
 
@@ -92,23 +55,15 @@ end
 ---          stays intact.                    ---
 -------------------------------------------------
 
-local strlen  = string.len
 local strchar = string.char
 local strbyte = string.byte
 local strsub  = string.sub
-local floor   = math.floor
-local bnot    = bit.bnot
-local band    = bit.band
-local bor     = bit.bor
-local bxor    = bit.bxor
-local shl     = bit.lshift
-local shr     = bit.rshift
 local h0, h1, h2, h3, h4
 
 -------------------------------------------------
 
 local function LeftRotate(val, nr)
-	return shl(val, nr) + shr(val, 32 - nr)
+	return lshift(val, nr) + rshift(val, 32 - nr)
 end
 
 -------------------------------------------------
@@ -117,13 +72,13 @@ local function ToHex(num)
 	local i, d
 	local str = ""
 	for i = 1, 8 do
-		d = band(num, 15)
+		d = num & 15
 		if (d < 10) then
 			str = strchar(d + 48) .. str
 		else
 			str = strchar(d + 87) .. str
 		end
-		num = floor(num / 16)
+		num = num // 16
 	end
 	return str
 end
@@ -133,9 +88,9 @@ end
 local function PreProcess(str)
 	local bitlen, i
 	local str2 = ""
-	bitlen = strlen(str) * 8
+	bitlen = #str * 8
 	str = str .. strchar(128)
-	i = 56 - band(strlen(str), 63)
+	i = 56 - (#str & 63)
 	if (i < 0) then
 		i = i + 64
 	end
@@ -143,8 +98,8 @@ local function PreProcess(str)
 		str = str .. strchar(0)
 	end
 	for i = 1, 8 do
-		str2 = strchar(band(bitlen, 255)) .. str2
-		bitlen = floor(bitlen / 256)
+		str2 = strchar(bitlen & 255) .. str2
+		bitlen = bitlen // 256
 	end
 	return str .. str2
 end
@@ -163,7 +118,7 @@ local function MainLoop(str)
 			end
 		end
 		for i = 16, 79 do
-			w[i] = LeftRotate(bxor(bxor(w[i - 3], w[i - 8]), bxor(w[i - 14], w[i - 16])), 1)
+			w[i] = LeftRotate(((w[i - 3] ~ w[i - 8]) ~ (w[i - 14] ~ w[i - 16])), 1)
 		end
 		a = h0
 		b = h1
@@ -172,16 +127,16 @@ local function MainLoop(str)
 		e = h4
 		for i = 0, 79 do
 			if (i < 20) then
-				f = bor(band(b, c), band(bnot(b), d))
+				f = (b & c) | ((~b) & d)
 				k = 1518500249
 			elseif (i < 40) then
-				f = bxor(bxor(b, c), d)
+				f = ((b ~ c) ~ d)
 				k = 1859775393
 			elseif (i < 60) then
-				f = bor(bor(band(b, c), band(b, d)), band(c, d))
+				f = (b & c) | (b & d) | (c & d)
 				k = 2400959708
 			else
-				f = bxor(bxor(b, c), d)
+				f = (b ~ c) ~ d
 				k = 3395469782
 			end
 			t = LeftRotate(a, 5) + f + e + k + w[i]	
@@ -191,11 +146,11 @@ local function MainLoop(str)
 			b = a
 			a = t
 		end
-		h0 = band(h0 + a, 4294967295)
-		h1 = band(h1 + b, 4294967295)
-		h2 = band(h2 + c, 4294967295)
-		h3 = band(h3 + d, 4294967295)
-		h4 = band(h4 + e, 4294967295)
+		h0 = (h0 + a) & 4294967295
+		h1 = (h1 + b) & 4294967295
+		h2 = (h2 + c) & 4294967295
+		h3 = (h3 + d) & 4294967295
+		h4 = (h4 + e) & 4294967295
 		str = strsub(str, 65)
 	end
 end
