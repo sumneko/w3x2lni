0dc7e8d402101c0685515a45673090a7bb24f135
最萌小汐 <sunyijiusyj@126.com>
2016-11-23 17:06:33 +0800

    更新txt字符串读取规则

--- src/create_template.lua
+++ src/create_template.lua
@@ -149,9 +149,10 @@ end
 -- 规则如下
 -- 1.如果第一个字符是逗号,则添加一个空串
 -- 2.如果最后一个字符是逗号,则添加一个空串
--- 3.如果当前字符为引号,则匹配到下一个引号,并忽略2端的字符
--- 4.如果当前字符为逗号,则忽略该字符.如果上一个字符是逗号,则添加一个空串
--- 5.否则匹配到下一个逗号或引号,并忽略该字符
+-- 3.如果最后一个字符是引号,则忽略该字符
+-- 4.如果当前字符为引号,则匹配到下一个引号,并忽略2端的字符
+-- 5.如果当前字符为逗号,则忽略该字符.如果上一个字符是逗号,则添加一个空串
+-- 6.否则匹配到下一个逗号或引号,并忽略该字符
 local function splite(str)
     local tbl = {}
     local cur = 1
@@ -160,6 +161,9 @@ local function splite(str)
     end
     while cur <= #str do
         if str:sub(cur, cur) == '"' then
+            if cur == #str then
+                break
+            end
             local pos = str:find('"', cur+1, true) or (#str+1)
             tbl[#tbl+1] = str:sub(cur+1, pos-1)
             cur = pos+1
