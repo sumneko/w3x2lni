4af4463e2834202ad5d8023e7e2ae77d34cf2d57
最萌小汐 <sunyijiusyj@126.com>
2016-12-02 21:11:38 +0800

    冲突解决错了

--- script/gui/main.lua
+++ script/gui/main.lua
@@ -173,13 +173,7 @@ local function window_mpq(canvas, height)
 		end
 		height = height - 68
 	end)
-	canvas:layout_row_dynamic(height, 1)
-	canvas:layout_row_dynamic(30, 1)
-	canvas:label(backend_lastmsg, NK_TEXT_LEFT)
-	canvas:layout_row_dynamic(10, 1)
-	canvas:layout_row_dynamic(30, 1)
-	canvas:progress(math.floor(backend_msgs['progress'] or 0), 100)
-	canvas:layout_row_dynamic(10, 1)
+	return height
 end
 
 local function window_dir(canvas, height)
