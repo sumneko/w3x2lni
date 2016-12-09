75e245d6dd22c1f9b511d4c2f3a4ecab332a374b
actboy168 <actboy168@gmail.com>
2016-12-09 15:14:11 +0800

    整理代码

--- script/prebuilt.lua
+++ script/prebuilt.lua
@@ -11,13 +11,6 @@ local create_key_type = require 'create_key_type'
 local order_prebuilt = require 'order.prebuilt'
 local table2lni = require 'table2lni'
 
-local rootpath = fs.path(uni.a2u(arg[0])):remove_filename()
-local mpq_dir = rootpath / 'script' / 'mpq'
-local key_dir = rootpath / 'script' / 'key'
-local root_dir = rootpath / 'script'
-local template_dir = rootpath / 'template'
-local default_dir = rootpath / 'script' / 'default'
-
 function message(...)
 	local tbl = {...}
 	local count = select('#', ...)
@@ -41,42 +34,42 @@ local function main()
 	w2l:init()
 
 	-- 生成key_type
-	local keydata = w2l:parse_txt(io.load(mpq_dir / 'ui' / 'uniteditordata.txt'))
+	local keydata = w2l:parse_txt(io.load(w2l.mpq / 'ui' / 'uniteditordata.txt'))
 	local content = create_key_type(keydata)
-	io.save(root_dir / 'key_type.lua', content)
+	io.save(w2l.root / 'script' / 'key_type.lua', content)
 
 	-- 生成key2id
     for file_name, meta in pairs(w2l.info['metadata']) do
 		message('正在生成key2id', file_name)
-		local metadata = w2l:read_metadata(mpq_dir / meta, io.load)
+		local metadata = w2l:read_metadata(w2l.mpq / meta, io.load)
         local slk = w2l.info['template']['slk'][file_name]
         local template = {}
         for i = 1, #slk do
-            add_table(template, w2l:parse_slk(io.load(mpq_dir / slk[i])))
+            add_table(template, w2l:parse_slk(io.load(w2l.mpq / slk[i])))
         end
 		local content = w2l:key2id(file_name, metadata, template)
-		io.save(key_dir / (file_name .. '.ini'), content)
+		io.save(w2l.key / (file_name .. '.ini'), content)
 	end
 
 	-- 生成模板lni
-	fs.create_directories(default_dir)
-	fs.create_directories(template_dir)
+	fs.create_directories(w2l.default)
+	fs.create_directories(w2l.template)
 	for file_name, meta in pairs(w2l.info['metadata']) do
 		message('正在生成模板', file_name)
 		local data = w2l:slk_loader(file_name, io.load, function(name)
 			return io.load(w2l.mpq / name)
 		end)
 		w2l:post_process(file_name, data, io.load)
-		io.save(default_dir / (file_name .. '.ini'), table2lni(data))
-		io.save(template_dir / (file_name .. '.ini'), w2l:to_lni(file_name, data, io.load))
-		local t = w2l:parse_lni(io.load(default_dir / (file_name .. '.ini')))
+		io.save(w2l.default / (file_name .. '.ini'), table2lni(data))
+		io.save(w2l.template / (file_name .. '.ini'), w2l:to_lni(file_name, data, io.load))
+		local t = w2l:parse_lni(io.load(w2l.default / (file_name .. '.ini')))
 		print(t)
 	end
 
 	-- 生成技能命令映射
-	local skill_data = w2l:parse_lni(io.load(template_dir / 'war3map.w3a.ini'))
+	local skill_data = w2l:parse_lni(io.load(w2l.template / 'war3map.w3a.ini'))
 	local order_list = order_prebuilt(skill_data)
-	io.save(rootpath / 'script' / 'order' / 'order_list.lua', order_list)
+	io.save(w2l.root / 'script' / 'order' / 'order_list.lua', order_list)
 
 	message('[完毕]: 用时 ' .. os.clock() .. ' 秒') 
 end
