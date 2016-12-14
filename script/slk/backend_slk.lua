f90298a710e674cfe4878cc747aff3d69e392df2
最萌小汐 <sunyijiusyj@126.com>
2016-12-14 23:03:36 +0800

    提交一下

--- script/slk/backend_slk.lua
+++ script/slk/backend_slk.lua
@@ -3,21 +3,45 @@ local w2l
 local metadata
 local keydata
 local keys
+local lines
 
-local function load_data(name, data, slk_data)
-    if not data then
+local function add_head(count)
+    lines[#lines+1] = 'ID;PWXL;N;E'
+    lines[#lines+1] = ('B;X%d;Y%d;D0'):format(#keys, count)
+end
+
+local function convert_slk()
+    if not next(slk) then
         return
     end
-    for i = 1, 4 do
-        slk_data[i] = data[i]
-        data[i] = nil
+    local names = {}
+    for name in pairs(slk) do
+        names[#names+1] = name
+    end
+    table.sort(names)
+    add_head(#names)
+    add_title()
+end
+
+local function load_data(name, obj, key, slk_data)
+    if not obj[key] then
+        return
+    end
+    if type(obj[key]) == 'table' then
+        for i = 1, 4 do
+            slk_data[key][i] = obj[key][i]
+            obj[key][i] = nil
+        end
+    else
+        slk_data[key] = obj[key]
+        obj[key] = nil
     end
 end
 
 local function load_obj(name, obj)
     local slk_data = {}
-    for _, key in ipairs(keydata) do
-        load_data(name, obj[key], slk_data)
+    for key in pairs(keys) do
+        load_data(name, obj, key, slk_data)
     end
     if next(slk_data) then
         return slk_data
@@ -33,9 +57,14 @@ end
 return function(w2l_, type, slk_name, chunk)
     slk = {}
     w2l = w2l_
+    lines = {}
     metadata = w2l:read_metadata(type)
     keydata = w2l:keyconvert(type)
     keys = keydata[slk_name]
 
     load_chunk(chunk)
+    convert_slk()
+    if #lines > 0 then
+        return table.concat(lines, '\r\n')
+    end
 end
