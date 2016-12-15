1b17372259abc4ee72303bc65635b70f1701aff4
actboy168 <actboy168@gmail.com>
2016-12-15 16:28:46 +0800

    obj数据的合并

--- script/slk/frontend.lua
+++ script/slk/frontend.lua
@@ -1,15 +1,54 @@
 local progress = require 'progress'
 
+local function merge(a, b)
+    for k, v in pairs(b) do
+        a[k] = v
+    end
+end
+
 local function table_merge(a, b)
     for k, v in pairs(b) do
-        if a[k] then
-            if type(a[k]) == 'table' and type(v) == 'table' then
-                table_merge(a[k], v)
-            else
-                a[k] = v
-            end
+        if a[k] and type(v) == 'table' then
+            merge(a[k], v)
         else
             a[k] = v
+        end
+    end
+end
+
+local function copy(a, b)
+    if b then
+        local c = {}
+        for k, v in pairs(a) do
+            c[k] = b[k] or v
+        end
+        return c
+    end
+    local c = {}
+    for k, v in pairs(a) do
+        c[k] = v
+    end
+    return c
+end
+
+local function table_copy(a, b)
+    local c = {}
+    for k, v in pairs(a) do
+        if type(v) == 'table' then
+            c[k] = copy(v, b[k])
+        else
+            c[k] = b[k] or v
+        end
+    end
+    return c
+end
+
+local function merge_obj(data, objs)
+    for name, obj in pairs(objs) do
+        if data[name] then
+            table_merge(data[name], obj)
+        else
+            data[name] = table_copy(data[obj._origin_id], obj)
         end
     end
 end
@@ -39,7 +78,7 @@ local function load(w2l, wts, archive, type, filename, target_progress)
     end
 
     if obj then
-        table_merge(data, obj)
+        merge_obj(data, obj)
     end
     return data
 end
