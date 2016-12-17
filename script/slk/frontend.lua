0bd0f105d17b304b9d265849daeb607b489e596c
最萌小汐 <sunyijiusyj@126.com>
2016-12-17 15:41:02 +0800

    整理一下代码

--- script/slk/frontend.lua
+++ script/slk/frontend.lua
@@ -75,29 +75,8 @@ local function merge_obj(data, objs)
 end
 
 local function load(w2l, wts, archive, type, target_progress)
-    local obj, data, force_slk
-
-    progress:target(target_progress-1)
-    local objname = w2l.info.template.obj[type]
-    local buf = archive:get(objname)
-    if buf then
-        message('正在转换', objname)
-        obj, force_slk = w2l:frontend_obj(type, wts, buf)
-    end
-
     progress:target(target_progress)
-    if force_slk or w2l.config.read_slk then
-        data = w2l:frontend_slk(type, function(name)
-            message('正在转换', name)
-            local buf = archive:get(name)
-            if buf then
-                return buf
-            end
-            return io.load(w2l.mpq / name)
-        end)
-    else
-        data = w2l:parse_lni(io.load(w2l.default / (type .. '.ini')))
-    end
+    
 
     if obj then
         merge_obj(data, obj)
@@ -105,12 +84,53 @@ local function load(w2l, wts, archive, type, target_progress)
     return data
 end
 
+local function load_slk(w2l, archive, force_slk)
+    local datas = {}
+    if force_slk or w2l.config.read_slk then
+        for type in pairs(w2l.info.template.slk) do
+            datas[type] = w2l:frontend_slk(type, function(name)
+                message('正在转换', name)
+                local buf = archive:get(name)
+                if buf then
+                    return buf
+                end
+                return io.load(w2l.mpq / name)
+            end)
+        end
+    else
+        for type in pairs(w2l.info.template.slk) do
+            datas[type] = w2l:parse_lni(io.load(w2l.default / (type .. '.ini')))
+        end
+    end
+    return datas
+end
+
+local function load_obj(w2l, archive, wts)
+    local objs = {}
+    local force_slk
+    for type, name in pairs(w2l.info.template.obj) do
+        local buf = archive:get(name)
+        local force
+        if buf then
+            message('正在转换', name)
+            objs[type], force = w2l:frontend_obj(type, wts, buf)
+            if force then
+                force_slk = true
+            end
+        end
+    end
+    return objs, force_slk
+end
+
 return function(w2l, archive, slk)
     --读取字符串
     slk.wts = w2l:frontend_wts(archive)
-    local target_progress = 3
-    for type in pairs(w2l.info.template.obj) do
-        target_progress = target_progress + 2
-        slk[type] = load(w2l, slk.wts, archive, type, target_progress)
+    local objs, force_slk = load_obj(w2l, archive, slk.wts)
+    local datas = load_slk(w2l, archive, force_slk)
+    for type, data in pairs(datas) do
+        if objs then
+            merge_obj(data, objs[type])
+        end
+        slk[type] = data
     end
 end
