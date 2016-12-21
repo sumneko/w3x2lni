8e4723df3d55e2f0a6dbcb7eb11b29a5530f5ca6
最萌小汐 <sunyijiusyj@126.com>
2016-12-21 22:26:22 +0800

    合并misc

--- script/slk/backend_misc.lua
+++ script/slk/backend_misc.lua
@@ -1,7 +1,71 @@
+local string_lower = string.lower
+local table_concat = table.concat
+
+local w2l
+local metadata
+local keydata
+
+local function to_type(tp, value)
+    if tp == 0 then
+        if not value then
+            return 0
+        end
+        return math_floor(wtonumber(value))
+    elseif tp == 1 or tp == 2 then
+        if not value then
+            return 0.0
+        end
+        return wtonumber(value) + 0.0
+    elseif tp == 3 then
+        if not value then
+            return nil
+        end
+        if value == '' then
+            return value
+        end
+        value = tostring(value)
+        if not value:match '[^ %-%_]' then
+            return nil
+        end
+        return value
+    end
+end
+
+local function merge_data(lkey, id, obj, txt_data)
+    if not txt_data[lkey] then
+        return
+    end
+    local meta = metadata[id]
+    local key = meta.field
+    local value = txt_data[lkey]
+    if meta.index == -1 then
+        value = table_concat(value, ',')
+    else
+        local tp = w2l:get_id_type(meta.type)
+        value = to_type(tp, value[1])
+    end
+    obj[key] = value
+end
+
+local function merge_obj(name, obj, txt)
+    local lname = string_lower(name)
+    local txt_data = txt[lname]
+    if not txt_data then
+        return
+    end
+    for lkey, id in pairs(keydata.common) do
+        merge_data(lkey, id, obj, txt_data)
+    end
+    if keydata[name] then
+        for lkey, id in pairs(keydata[name]) do
+            merge_data(lkey, id, obj, txt_data)
+        end
+    end
+end
+
 local function merge_constant(misc, txt)
-    for _, name in ipairs {'TWN2', 'TWN3', 'HERO', 'TALT', 'TWN1'} do
-        local lname = name:lower()
-        misc[name].Name = txt[lname].name[1]
+    for name, obj in pairs(misc) do
+        merge_obj(name, obj, txt)
     end
 end
 
@@ -37,7 +101,10 @@ local function convert(misc, wts)
     return table.concat(lines, '\r\n')
 end
 
-return function(w2l, misc, txt, wts)
+return function(w2l_, misc, txt, wts)
+    w2l = w2l_
+    metadata = w2l:read_metadata 'misc'
+    keydata = w2l:parse_lni(io.load(w2l.key / 'misc.ini'))
     merge_constant(misc, txt)
     local buf = convert(misc, wts)
     return buf
