3c9accc4d2e116b9310258c80413a7bcc77963ba
actboy168 <actboy168@gmail.com>
2016-12-27 10:55:08 +0800

    更新日志的切换

--- script/gui/changelog.lua
+++ script/gui/changelog.lua
@@ -8,11 +8,34 @@ local color  = {
 	UI = {111, 77, 150},
 }
 
-local function version(msg)
-	canvas:layout_space(25, 1)
-	canvas:layout_space_push(0, 0, 80, 25) 
-	window:set_style('button.color', 81, 85, 97)
-	canvas:button(msg)
+local current
+local index
+
+local function version_begin()
+	canvas:layout_space(25, 4)
+	index = 0
+end
+
+local function version(ver, text)
+	if not current then
+		current = text
+	end
+	if current == text then
+		window:set_style('button.color', 131, 135, 147)
+	else
+		window:set_style('button.color', 81, 85, 97)
+	end
+	index = index + 1
+	canvas:layout_space_push(index * 80 - 80, 0, 80, 25)
+	if canvas:button(ver) then
+		current = text
+	end
+end
+
+local function version_end()
+	if current then
+		current()
+	end
 end
 
 local function log(type, msg, tip)
@@ -23,17 +46,23 @@ local function log(type, msg, tip)
 end
 
 return function(window_, canvas_)
-    window = window_
-    canvas = canvas_
-	version('1.1')
-	log('NEW', '支持模型压缩', '有损压缩')
-	log('NEW', '无法放在txt中的字符串会放在wts里', '尽量不要同时包含逗号和双引号')
-	log('NEW', '增加部分选项的提示')
-	log('NEW', '转换成OBJ时会补充必要文件', 'war3mapunits.doo')
-	log('CHG', '重要的详情现在会更加显眼')
-	log('CHG', '必须保留列表移除一些对象', 'Bbar、Bchd、Buad、Biwb')
-	log('FIX', '修正某些格式互转地图不可用的问题')
-	log('FIX', '修正无法读取南瓜头生成的txt的问题')
-	log('FIX', '修正读取0级技能会出错的问题')
-	log('FIX', '修正详情里的tip被截断的问题')
+	window = window_
+	canvas = canvas_
+	version_begin()
+	version('1.2', function()
+		log('FIX', '修正lpeg模块加载失败的问题')
+	end)
+	version('1.1', function()
+		log('NEW', '支持模型压缩', '有损压缩')
+		log('NEW', '无法放在txt中的字符串会放在wts里', '尽量不要同时包含逗号和双引号')
+		log('NEW', '增加部分选项的提示')
+		log('NEW', '转换成OBJ时会补充必要文件', 'war3mapunits.doo')
+		log('CHG', '重要的详情现在会更加显眼')
+		log('CHG', '必须保留列表移除一些对象', 'Bbar、Bchd、Buad、Biwb')
+		log('FIX', '修正某些格式互转地图不可用的问题')
+		log('FIX', '修正无法读取南瓜头生成的txt的问题')
+		log('FIX', '修正读取0级技能会出错的问题')
+		log('FIX', '修正详情里的tip被截断的问题')
+	end)
+	version_end()
 end
