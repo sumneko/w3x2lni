721eb1d6e1d7b49e2af41bf91549f7578e5bba9c
actboy168 <actboy168@gmail.com>
2017-01-05 11:20:48 +0800

    生成lni时强制类型转换

--- script/slk/backend_lni.lua
+++ script/slk/backend_lni.lua
@@ -24,17 +24,15 @@ local function get_len(tbl)
     return n
 end
 
-local function format_value(value)
-    local tp = type(value)
-    if tp == 'number' then
-        if math_type(value) == 'integer' then
-            return ('%d'):format(value)
-        else
-            return ('%.4f'):format(value)
-        end
-    elseif tp == 'nil' then
+local function format_value(tp, value)
+    if value == nil then
         return 'nil'
-    else
+    end
+    if tp == 0 then
+        return ('%d'):format(value)
+    elseif tp == 1 or tp == 2 then
+        return ('%.4f'):format(value)
+    elseif tp == 3 then
         value = w2l:editstring(value)
         if value:match '[\n\r]' then
             return ('[=[\r\n%s]=]'):format(value)
@@ -42,6 +40,7 @@ local function format_value(value)
             return ('%q'):format(value)
         end
     end
+    return 'nil'
 end
 
 local function write(format, ...)
@@ -62,11 +61,11 @@ local function write_data(meta, data, lines)
     end
     lines[#lines+1] = {'-- %s', w2l:editstring(meta.displayname):gsub('^%s*(.-)%s*$', '%1')}
     if not len then
-        lines[#lines+1] = {'%s = %s', key, format_value(data)}
+        lines[#lines+1] = {'%s = %s', key, format_value(meta.type, data)}
         return
     end
     if len <= 1 then
-        lines[#lines+1] = {'%s = %s', key, format_value(data[1])}
+        lines[#lines+1] = {'%s = %s', key, format_value(meta.type, data[1])}
         return
     end
 
@@ -77,9 +76,9 @@ local function write_data(meta, data, lines)
             is_string = true
         end
         if len >= 10 then
-            values[i] = ('%d = %s'):format(i, format_value(data[i]))
+            values[i] = ('%d = %s'):format(i, format_value(meta.type, data[i]))
         else
-            values[i] = format_value(data[i])
+            values[i] = format_value(meta.type, data[i])
         end
     end
 
@@ -91,28 +90,23 @@ local function write_data(meta, data, lines)
     lines[#lines+1] = {'%s = {%s}', key, table_concat(values, ', ')}
 end
 
-local function write_obj(obj)
-    if remove_unuse_object and not obj._mark then
-        return
-    end
-    local datas = {}
+local function write_obj(id, obj)
     local metas = {}
-    local keys = {}
-    local name = obj._id
-    local code = obj._code
-    if metadata[code] then
-        for key, meta in pairs(metadata[code]) do
+    local datas = {}
+    local haskey = {}
+    if metadata[obj._code] then
+        for key, meta in pairs(metadata[obj._code]) do
             local data = obj[key]
             if data then
                 metas[#metas+1] = meta
                 datas[meta] = data
             end
-            keys[key] = true
+            haskey[key] = true
         end
     end
     if metadata[ttype] then
         for key, meta in pairs(metadata[ttype]) do
-            if not keys[key] then
+            if not haskey[key] then
                 local data = obj[key]
                 if data then
                     metas[#metas+1] = meta
@@ -128,11 +122,10 @@ local function write_obj(obj)
     for _, meta in ipairs(metas) do
         write_data(meta, datas[meta], lines)
     end
-    if not lines or #lines == 0 then
+    if #lines == 0 then
         return
     end
-
-    write('[%s]', obj._id)
+    write('[%s]', id)
     if obj._parent then
         write('%s = %q', '_parent', obj._parent)
     end
@@ -143,11 +136,13 @@ local function write_obj(obj)
 end
 
 local function write_table(slk)
-    local names = {}
-    for name, obj in pairs(slk) do
-        table_insert(names, name)
+    local list = {}
+    for id in pairs(slk) do
+        if not remove_unuse_object or obj._mark then
+            list[#list+1] = id
+        end
     end
-    table_sort(names, function(a, b)
+    table_sort(list, function(a, b)
         local is_origin_a = a == slk[a]._parent
         local is_origin_b = b == slk[b]._parent
         if is_origin_a and not is_origin_b then
@@ -156,16 +151,16 @@ local function write_table(slk)
         if not is_origin_a and is_origin_b then
             return false
         end
-        return slk[a]._id < slk[b]._id
+        return a < b
     end)
     local clock = os_clock()
-    for i = 1, #names do
-        local obj = slk[names[i]]
-        write_obj(obj)
+    for i = 1, #list do
+        local obj = slk[list[i]]
+        write_obj(list[i], obj)
         if os_clock() - clock >= 0.1 then
             clock = os_clock()
-            message(('正在转换%s: [%s] (%d/%d)'):format(ttype, obj._id, i, #names))
-            progress(i / #names)
+            message(('正在转换%s: [%s] (%d/%d)'):format(ttype, obj._id, i, #list))
+            progress(i / #list)
         end
     end
 end
