31684c69af5b1e302093acb5a429b419ceb76de0
最萌小汐 <sunyijiusyj@126.com>
2017-01-09 15:56:19 +0800

    修改wts解析

--- script/slk/frontend_wts.lua
+++ script/slk/frontend_wts.lua
@@ -4,7 +4,7 @@ local mt = {}
 mt.__index = mt
 
 -- TODO: 同时有英文逗号和英文双引号的字符串存在txt里会解析出错
---       包含右打括号的字符串存在wts里会解析出错
+--       包含右大括号的字符串存在wts里会解析出错
 --       超过256字节的字符串存在二进制里会崩溃
 function mt:load(content, max)
     local wts = self.wts
@@ -53,18 +53,82 @@ function mt:refresh()
     return table.concat(lines, '\r\n\r\n')
 end
 
+local function search_string(buf, callback)
+    local lines = {}
+    local current = 1
+    while true do
+        local start, finish = buf:find('\r\n', current, false)
+        if start then
+            lines[#lines+1] = buf:sub(current, start-1)
+            current = finish + 1
+        else
+            lines[#lines+1] = buf:sub(current, #buf)
+            break
+        end
+    end
+    if lines[1]:sub(1, 3) == '\xEF\xBB\xBF' then
+        lines[1] = lines[1]:sub(4)
+    end
+    for i = #lines, 1, -1 do
+        if lines[i] == '' then
+            lines[i] = nil
+        else
+            break
+        end
+    end
+    local count = 1
+    while count <= #lines do
+        local index = tonumber(lines[count]:match('^STRING (%d+)$'))
+        if index then
+            if lines[count+1]:sub(1, 2) == '//' then
+                count = count + 1
+            end
+            if lines[count+1] ~= '{' then
+                message('-report|2警告', ('wts解析错误:(%d) %s'):format(count, '缺少"{"'))
+                message('-tip', lines[count])
+                return
+            end
+            local suc
+            for i = count+2, #lines do
+                if lines[i] == '}' and
+                   (lines[i+1] == nil or lines[i+1] == '') and
+                   (lines[i+2] == nil or lines[i+2]:match('^STRING (%d+)$'))
+                then
+                    local text = table.concat(lines, '\n\r', count+2, i-1)
+                    callback(index, text)
+                    count = i + 2
+                    suc = true
+                    break
+                end
+            end
+            if not suc then
+                message('-report|2警告', ('wts解析错误:(%d) %s'):format(count, '缺少"}"'))
+                message('-tip', lines[count])
+                return
+            end
+        else
+            count = count + 1
+        end
+    end
+end
+
 -- TODO: 待重构，数据和操作分离 
 return function (w2l, archive)
-    local buf = archive:get('war3map.wts') or ''
+    local buf = archive:get('war3map.wts')
     local tbl = {}
-    for string in buf:gmatch 'STRING.-%c*%}' do
-        local i, s = string:match 'STRING (%d+).-%{\r\n(.-)%\r\n}'
-        local t    = {
-            index    = i,
-            text    = s,
-        }
-        table_insert(tbl, t)
-        tbl[('%03d'):format(i)] = t    --这里的索引是字符串
+    if buf then
+        search_string(buf, function(index, text)
+            if text:find('}', 1, false) then
+                message('-report|2警告', '文本不能包含字符"}"')
+                message('-tip', text:gsub('\n\r', ' '))
+            end
+            local t = {
+                index = index,
+                text  = text,
+            }
+            table_insert(tbl, t)
+            tbl[('%03d'):format(index)] = t    --这里的索引是字符串
+        end)
     end
     return setmetatable({ wts = tbl, lastindex = 0 }, mt)
 end
