37788dc5e5b60b520ff3a615ebf7cc144ce2a6c6
最萌小汐 <sunyijiusyj@126.com>
2017-05-08 16:49:52 +0800

    使用更搞笑的匹配方式

--- script/slk/backend_convertjass.lua
+++ script/slk/backend_convertjass.lua
@@ -1,5 +1,4 @@
 local lpeg = require 'lpeg'
-local line_count
 local w2l
 local wts
 
@@ -18,15 +17,17 @@ local Cs = lpeg.Cs
 
 local nl1  = P'\r\n' + S'\r\n'
 local com  = P'//' * (1-nl1)^0
-local nl   = com^0 * nl1 / function() line_count = line_count + 1 end
+local nl   = com^0 * nl1
 local quo  = P'"'
 local esc  = P'\\'
 local num  = R'09'
 local str1 = esc * P(1) + (1-quo)
 local str  = quo * (nl1 + str1)^0 * quo
-local wst  = quo * Cs('TRIGSTR_' * num^3 / fwts) * quo
+local wst  = quo * ('TRIGSTR_' * num^3 / fwts) * quo
+local mtch = nl + wst + str
+local any  = 1 - mtch
 
-local pjass = C((nl + wst + str + 1)^0)
+local pjass = Cs((mtch + any^1)^0)
 
 return function (w2l_, archive, wts_)
     local name = 'war3map.j'
@@ -40,6 +41,5 @@ return function (w2l_, archive, wts_)
     end
     w2l = w2l_
     wts = wts_
-    line_count = 0
     archive:set(name, pjass:match(buf))
 end
