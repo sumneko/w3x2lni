c6c665a240f4298ca4aaba8fa6fa831720b00f2b
最萌小汐 <76196625@qq.com>
2017-12-22 16:37:30 +0800

    暂时先这样解决bat看不到error的问题

--- script/main.lua
+++ script/main.lua
@@ -1,15 +1,21 @@
 local function run_from_bat()
     local nk = require 'nuklear'
     nk:console()
+    local std_print = print
     local cmd = table.remove(arg, 1)
-    if cmd == '-prebuilt' then
-        require 'prebuilt.init'
-    elseif cmd == '-slk' then
-        require 'slk'
-    elseif cmd == '-mpq' then
-        require 'custom_mpq'
-    elseif cmd == '-fix_wtg' then
-        require 'fix_wtg'
+    local suc, msg = xpcall(function()
+        if cmd == '-prebuilt' then
+            require 'prebuilt.init'
+        elseif cmd == '-slk' then
+            require 'slk'
+        elseif cmd == '-mpq' then
+            require 'custom_mpq'
+        elseif cmd == '-fix_wtg' then
+            require 'fix_wtg'
+        end
+    end, debug.traceback)
+    if not suc then
+        std_print(msg)
     end
     os.execute('pause')
 end
