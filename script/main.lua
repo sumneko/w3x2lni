0a9bc8289fe33ce67fc74330b424e7a0e9681bb0
最萌小汐 <76196625@qq.com>
2017-12-22 16:29:29 +0800

    bat启动走一个统一的入口

--- script/main.lua
+++ script/main.lua
@@ -1,30 +1,24 @@
+local function run_from_bat()
+    local nk = require 'nuklear'
+    nk:console()
+    local cmd = table.remove(arg, 1)
+    if cmd == '-prebuilt' then
+        require 'prebuilt.init'
+    elseif cmd == '-slk' then
+        require 'slk'
+    elseif cmd == '-mpq' then
+        require 'custom_mpq'
+    elseif cmd == '-fix_wtg' then
+        require 'fix_wtg'
+    end
+    os.execute('pause')
+end
+
 if arg[1] == '-backend' then
     table.remove(arg, 1)
     require 'map'
-elseif arg[1] == '-prebuilt' then
-    local nk = require 'nuklear'
-    nk:console()
-    table.remove(arg, 1)
-    local prebuilt = require 'prebuilt.init'
-    os.execute('pause')
-elseif arg[1] == '-slk' then
-    local nk = require 'nuklear'
-    nk:console()
-    table.remove(arg, 1)
-    require 'slk'
-    os.execute('pause')
-elseif arg[1] == '-mpq' then
-    local nk = require 'nuklear'
-    nk:console()
-    table.remove(arg, 1)
-    require 'custom_mpq'
-    os.execute('pause')
-elseif arg[1] == '-fix_wtg' then
-    local nk = require 'nuklear'
-    nk:console()
-    table.remove(arg, 1)
-    require 'fix_wtg'
-    os.execute('pause')
-else
+elseif arg[1] == nil then
     require 'gui.main'
+else
+    run_from_bat()
 end
