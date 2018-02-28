b9c4d01d536371b2d3cea51131f5f0a35779d23d
最萌小汐 <76196625@qq.com>
2018-02-28 15:21:11 +0800

    换个列表结束符

--- script/core/slk/backend_eca.lua
+++ script/core/slk/backend_eca.lua
@@ -104,11 +104,10 @@ function convert_action(action, sp, in_arg)
     end
     
     if action.child then
-        lines[#lines+1] = (' '):rep(sp) .. '{'
         for type, actions in pairs_child(action.child) do
             convert_child(type_map[type], actions, sp+2)
         end
-        lines[#lines+1] = (' '):rep(sp) .. '}'
+        lines[#lines+1] = ('%s<%s>'):format((' '):rep(sp+2), '/列表')
     end
 end
 
