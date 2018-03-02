1af9d24c050eb012490d4c9139790b63c94bfe0a
最萌小汐 <76196625@qq.com>
2018-03-02 13:07:28 +0800

    转换成lml格式

--- script/core/slk/backend_eca.lua
+++ script/core/slk/backend_eca.lua
@@ -7,6 +7,7 @@ local arg_type_map = {
 }
 
 local type_map = {
+    [-1] = '列表',
     [0] = '事件',
     [1] = '条件',
     [2] = '动作',
@@ -42,14 +43,25 @@ local function pairs_child(child)
     local childs = {}
     local id_map = {}
     local keys = {}
+    local max_id = 0
     for _, eca in ipairs(child) do
         local id = eca.child_id
         if not childs[id] then
             childs[id] = {}
             id_map[id] = eca.type
             keys[#keys+1] = id
+            if id > max_id then
+                max_id = id
+            end
         end
         childs[id][#childs[id]+1] = eca
+    end
+    for id = 0, max_id-1 do
+        if not childs[id] then
+            childs[id] = {}
+            id_map[id] = -1
+            keys[#keys+1] = id
+        end
     end
     table.sort(keys)
     local index = 0
@@ -66,59 +78,51 @@ local function parse_arg(arg)
     if arg.eca then
         return parse_eca(arg.eca, true)
     elseif arg.index then
-        return { [arg.value] = { '数组', parse_arg(arg.index) }}
+        return {arg.value, '数组', parse_arg(arg.index) }
     else
-        return { [arg.value] = arg_type_map[arg.type] }
+        return {arg.value, arg_type_map[arg.type]}
     end
 end
 
 function parse_eca(eca, in_arg)
     local action = {}
+    action[1] = eca.name
 
     if in_arg then
-        action[#action+1] = type_map[eca.type]
-    end
-    if eca.enable == 0 then
-        action[#action+1] = '禁用'
+        action[2] = type_map[eca.type]
+    elseif eca.enable == 0 then
+        action[2] = '禁用'
+    else
+        action[2] = false
     end
     if eca.args then
-        for _, arg in ipairs(eca.args) do
-            action[#action+1] = parse_arg(arg)
+        for i, arg in ipairs(eca.args) do
+            action[i+2] = parse_arg(arg)
         end
     end
     if eca.child then
         for name, ecas in pairs_child(eca.child) do
-            local child = {}
+            local child = { type_map[name], false }
             for i, eca in ipairs(ecas) do
-                child[i] = parse_eca(eca)
+                child[i+2] = parse_eca(eca)
             end
-            action[#action+1] = { [type_map[name]] = child }
+            action[#action+1] = child
         end
     end
 
-    if #action == 0 then
-        return eca.name
-    elseif #action == 1 and type(action[1]) ~= 'table' then
-        return { [eca.name] = action[1] }
-    else
-        return { [eca.name] = action }
-    end
+    return action
 end
 
 local function parse_trg(ecas)
-    local trg = sort_table()
-    trg['事件'] = {}
-    trg['条件'] = {}
-    trg['动作'] = {}
+    local trg = {
+        { '事件', false },
+        { '条件', false },
+        { '动作', false },
+    }
 
     for _, eca in ipairs(ecas) do
-        local tp = type_map[eca.type]
-        trg[tp][#trg[tp]+1] = parse_eca(eca)
-    end
-    for k, v in pairs(trg) do
-        if #v == 0 then
-            trg[k] = nil
-        end
+        local t = trg[eca.type+1]
+        t[#t+1] = parse_eca(eca)
     end
     
     return trg
@@ -143,10 +147,8 @@ local sp_rep = setmetatable({}, {
 })
 
 local function yaml_string(str)
-    if str == nil then
-        str = '~'
-    elseif type(str) == 'string' then
-        if tonumber(str) or find(str, "[%s%:%'%-%c]") then
+    if type(str) == 'string' then
+        if find(str, "[%s%:%'%c]") then
             str = format("'%s'", gsub(str, "'", "''"))
         end
     else
@@ -155,39 +157,22 @@ local function yaml_string(str)
     return str
 end
 
-local function yaml_array(v, level, sp)
-    for i, v in ipairs(v) do
-        if type(v) == 'table' then
-            buf[#buf+1] = format('%s- ', sp)
-            yaml_table(v, level, '')
-        else
-            buf[#buf+1] = format('%s- %s\n', sp, yaml_string(v))
-        end
-    end
-end
-
-local function yaml_hash(v, level, sp)
-    for k, v in pairs(v) do
-        if type(v) == 'table' then
-            buf[#buf+1] = format('%s%s:\n', sp, yaml_string(k))
-            yaml_table(v, level+4, sp_rep[level+4])
-        else
-            buf[#buf+1] = format('%s%s: %s\n', sp, yaml_string(k), yaml_string(v))
-        end
-    end
-end
-
-function yaml_table(v, level, sp)
-    if #v > 0 then
-        yaml_array(v, level, sp)
+local function yaml_value(v, sp)
+    if v[2] then
+        buf[#buf+1] = format('%s%s: %s\n', sp_rep[sp], yaml_string(v[1]), yaml_string(v[2]))
     else
-        yaml_hash(v, level, sp)
+        buf[#buf+1] = format('%s%s\n', sp_rep[sp], yaml_string(v[1]))
+    end
+    for i = 3, #v do
+        yaml_value(v[i], sp+4)
     end
 end
 
 local function convert_yaml(tbl)
     buf = {}
-    yaml_table(tbl, 0, '')
+    for _, v in ipairs(tbl) do
+        yaml_value(v, 0)
+    end
     return table.concat(buf)
 end
 
