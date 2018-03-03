ae0169367da61455b639bb3a28521a408f5095c9
最萌小汐 <76196625@qq.com>
2018-03-03 17:34:14 +0800

    删除多余代码

--- script/core/slk/backend_wtg2lml.lua
+++ script/core/slk/backend_wtg2lml.lua
@@ -2,32 +2,6 @@ local w2l
 local wtg
 local wct
 
-local function sort_table()
-    local keys = {}
-    local mark = {}
-    return setmetatable({}, {
-        __newindex = function (self, key, value)
-            rawset(self, key, value)
-            if not mark[key] then
-                mark[key] = true
-                keys[#keys+1] = key
-            end
-        end,
-        __pairs = function (self)
-            local i = 0
-            local function next()
-                i = i + 1
-                local k = keys[i]
-                if k ~= nil and self[k] == nil then
-                    return next()
-                end
-                return k, self[k]
-            end
-            return next
-        end,
-    })
-end
-
 local function get_path(path, used)
     path = path:gsub('[$\\$/$:$*$?$"$<$>$|]', '_')
     while used[path] do
@@ -41,84 +15,6 @@ local function get_path(path, used)
         path = name .. '_' .. id
     end
     return path
-end
-
-local function format_value(v)
-    local tp = type(v)
-    if tp == 'string' then
-        if v:find('[\n\r]') then
-            v = ('[[\n%s]]'):format(v)
-        elseif not v:find('%D') then
-            v = ("'%s'"):format(v)
-        end
-    end
-    return v
-end
-
-local function format_value2(v)
-    local tp = type(v)
-    if tp == 'string' then
-        if v:find('[\n\r]') then
-            v = ('[[\n%s]]'):format(v)
-        elseif not v:find("'") then
-            v = ("'%s'"):format(v)
-        elseif not v:find('"') then
-            v = ('"%s"'):format(v)
-        else
-            v = ('[[\n%s]]'):format(v)
-        end
-    end
-    return v
-end
-
-local function write_array(lines, name, data)
-    lines[#lines+1] = ('%s = {'):format(name)
-    for _, v in ipairs(data) do
-        lines[#lines+1] = ('%s,'):format(format_value2(v))
-    end
-    lines[#lines+1] = '}'
-end
-
-local function write_obj(lines, name, data)
-    lines[#lines+1] = ('[%s]'):format(name)
-    for k, v in pairs(data) do
-        if type(v) == 'table' then
-            if #v == 0 then
-                local new_name
-                if name:find('.', 1, true) then
-                    new_name = name .. '.' .. k
-                else
-                    new_name = '.' .. k
-                end
-                write_obj(lines, new_name, v)
-            else
-                write_array(lines, k, v)
-            end
-        else
-            lines[#lines+1] = ('%s = %s'):format(k, format_value(v))
-        end
-    end
-end
-
-local function write_lni(data)
-    local lines = {}
-    local tbls = sort_table()
-    local root = sort_table()
-    for name, data in pairs(data) do
-        if type(data) == 'table' then
-            tbls[name] = data
-        else
-            root[name] = data
-        end
-    end
-    if next(root) then
-        write_obj(lines, 'root', root)
-    end
-    for name, data in pairs(tbls) do
-        write_obj(lines, name, data)
-    end
-
-    return table.concat(lines, '\n')
 end
 
 local function compute_path()
