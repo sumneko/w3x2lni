2bcc0ab8bf9060d72b1a9d74d1e27ee969970a41
最萌小汐 <76196625@qq.com>
2018-03-06 17:55:26 +0800

    计算exe路径放到main里

--- script/map.lua
+++ script/map.lua
@@ -1,8 +1,3 @@
-(function()
-    local exepath = package.cpath:sub(1, (package.cpath:find(';') or 0)-6)
-    package.path = package.path .. ';' .. exepath .. '..\\script\\?.lua;' .. exepath .. '..\\script\\?\\init.lua;' .. exepath .. '..\\script\\core\\?.lua;' .. exepath .. '..\\script\\core\\?\\init.lua'
-end)()
-
 require 'filesystem'
 require 'utility'
 local uni = require 'ffi.unicode'
