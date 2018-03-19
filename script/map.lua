830569779244265b8f2992c49e3ead10cfac2a69
最萌小汐 <76196625@qq.com>
2018-03-19 15:32:15 +0800

    整理一下代码

--- script/map.lua
+++ script/map.lua
@@ -200,6 +200,20 @@ function w2l:trigger_data()
     return triggerdata()
 end
 
+local function save_builder(doo)
+    if w2l.config.mode == 'lni' then
+        local path = output / 'builder.w3x'
+        local ex_map = builder.load(path, 'w')
+        ex_map:set('war3mapunits.doo', w2l:create_unitsdoo())
+        ex_map:set('war3map.doo', doo)
+        ex_map:set('war3map.w3e', w2l:create_w3e())
+        ex_map:set('war3map.w3i', w2l:backend_w3i(slk.w3i, slk.wts))
+        slk.w3i['地图']['地图名称'] = 'W2L\x01'
+        ex_map:save(slk.w3i, w2l.progress, false)
+        ex_map:close()
+    end
+end
+
 print('正在读取物编...')
 w2l.progress:start(0.4)
 w2l:frontend(slk)
@@ -214,16 +228,7 @@ print('正在生成文件...')
 local doo = input_ar:get 'war3map.doo'
 w2l.progress:start(1)
 builder.save(w2l, output_ar, slk.w3i, input_ar)
-w2l.progress:finish()
-if w2l.config.mode == 'lni' then
-    local path = output / 'builder.w3x'
-    local ex_map = builder.load(path, 'w')
-    ex_map:set('war3mapunits.doo', w2l:create_unitsdoo())
-    ex_map:set('war3map.doo', doo)
-    ex_map:set('war3map.w3e', w2l:create_w3e())
-    ex_map:set('war3map.w3i', w2l:backend_w3i(slk.w3i, slk.wts))
-    slk.w3i['地图']['地图名称'] = 'W2L\x01'
-    ex_map:save(slk.w3i, w2l.progress, false)
-    ex_map:close()
-end
+w2l.progress:finish(doo)
+
+save_builder()
 print('转换完毕,用时 ' .. os.clock() .. ' 秒') 
