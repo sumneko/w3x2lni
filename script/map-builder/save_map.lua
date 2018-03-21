e1a40380ebf8c545b6967792b627f79f555d1f4b
最萌小汐 <76196625@qq.com>
2018-03-21 21:09:34 +0800

    修正连续转换lni没有重新保存imp的问题

--- script/map-builder/save_map.lua
+++ script/map-builder/save_map.lua
@@ -64,10 +64,12 @@ return function (w2l, output_ar, w3i, input_ar)
     end
     w2l:map_remove('builder.w3x')
     if w2l.config.mode == 'lni' then
-        if not w2l:file_load('lni', 'imp') and w2l:file_load('map', 'war3map.imp') then
+        if w2l:file_load('map', 'war3map.imp') then
             w2l:file_save('lni', 'imp', w2l:backend_imp(w2l:file_load('map', 'war3map.imp')))
+            w2l:file_remove('map', 'war3map.imp')
+        else
+            w2l:file_save('lni', 'imp', w2l:file_load('lni', 'imp'))
         end
-        w2l:file_remove('map', 'war3map.imp')
     else
         if w2l.config.remove_we_only then
             w2l:file_remove('map', 'war3map.imp')
