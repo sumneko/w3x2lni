a5f7c373bab1bfbb4917ced22750b837b4bbf5f1
最萌小汐 <76196625@qq.com>
2018-03-28 16:29:44 +0800

    前端支持新的插件格式

---
+++ plugin/示例.lua
@@ -0,0 +1,17 @@
+local mt = {}
+
+mt.info = {
+    version = 1.0,
+    author = '最萌小汐',
+    description = '在slk时，将所有单位的名字加上前缀"被插件修改过的"。',
+}
+
+function mt:on_complete_data(w2l)
+    if w2l.config.mode == 'slk' then
+        for id, obj in pairs(w2l.slk.unit) do
+            obj.name = '被插件修改过的' .. obj.name
+        end
+    end
+end
+
+return mt
