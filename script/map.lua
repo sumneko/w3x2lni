58cdafecc9196903df4e17145f4bd4f6a2ad785a
最萌小汐 <sumneko@hotmail.com>
2018-03-30 16:58:30 +0800

    把地图关掉

--- script/map.lua
+++ script/map.lua
@@ -82,6 +82,7 @@ local function check_lni_mark(path)
     if map then
         map:seek('set', 8)
         local mark = map:read(4)
+        map:close()
         if mark == 'W2L\x01' then
             return true
         end
