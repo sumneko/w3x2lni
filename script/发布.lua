52e69d1a3d38bafa83f7f6d67821b87e204e47a4
最萌小汐 <sumneko@hotmail.com>
2018-03-30 18:26:45 +0800

    发布工程

--- script/发布.lua
+++ script/发布.lua
@@ -1,9 +1,12 @@
 package.path = package.path .. ';script\\?.lua'
 
 require 'filesystem'
+local process = require 'process'
 local sleep = require 'ffi.sleep'
+local uni = require 'ffi.unicode'
 
 local root = fs.path(arg[1])
+local release_path
 
 local function task(f, ...)
     for i = 1, 99 do
@@ -20,50 +23,94 @@ local function read_version()
     return chg[1].version
 end
 
-local function create_directory(path)
-    if fs.exists(path) then
-        task(fs.remove_all, path)
+local function create_directory()
+    print('正在清空目录：', release_path)
+    if fs.exists(release_path) then
+        if not task(fs.remove_all, release_path) then
+            error(('清空目录失败：%s'):format(release_path))
+        end
     end
-    fs.create_directory(path)
+    fs.create_directory(release_path)
 end
 
-local function copy_files(path, input)
+local function copy_files(input)
+    print('正在复制文件：', input)
     local function f (name)
         if fs.is_directory(root / name) then
             for new in (root / name):list_directory() do
                 f(name / new:filename())
             end
         else
-            fs.create_directories((path / name):parent_path())
-            local suc, err = pcall(fs.copy_file, root / name, path / name, true)
+            fs.create_directories((release_path / name):parent_path())
+            local suc, err = pcall(fs.copy_file, root / name, release_path / name, true)
             if not suc then
-                error(('复制文件失败：[%s] -> [%s]\n'):format(root / name, path / name))
+                error(('复制文件失败：[%s] -> [%s]\n%s'):format(root / name, release_path / name, err))
             end
         end
     end
     f(fs.path(input))
 end
 
+local function remove_files(input)
+    print('正在删除文件：', input)
+    if fs.exists(release_path / input) then
+        if not task(fs.remove_all, release_path / input) then
+            error(('清空目录失败：%s'):format(release_path / input))
+        end
+    end
+end
+
+local function unit_test()
+    local application = release_path / 'bin' / 'w2l-worker.exe'
+    local entry = release_path / 'script' / 'test' / 'unit_test.lua'
+    local currentdir = release_path / 'script'
+    local command_line = ('"%s" "%s"'):format(application:string(), entry:string())
+    local p = process()
+	p:hide_window()
+	local stdout, stderr = p:std_output(), p:std_error()
+	if not p:create(application, command_line, currentdir) then
+		error('运行失败：\n'..command_line)
+    end
+    while true do
+        local out = stdout:read 'l'
+        if out then
+            print(uni.a2u(out))
+        else
+            break
+        end
+    end
+    local err = stderr:read 'a'
+    local exit_code = p:wait()
+    p:close()
+    if err ~= '' then
+        print(err)
+    end
+end
+
 local version = read_version()
-local release_path = root / ('w3x2lni_'..version)
-create_directory(release_path)
-copy_files(release_path, 'bin')
-copy_files(release_path, 'data')
-copy_files(release_path, 'plugin/本地脚本.lua')
-copy_files(release_path, 'plugin/模型加密.lua')
-copy_files(release_path, 'plugin/示例.lua')
-copy_files(release_path, 'plugin/资源保护.lua')
-copy_files(release_path, 'script/core')
-copy_files(release_path, 'script/ffi')
-copy_files(release_path, 'script/gui')
-copy_files(release_path, 'script/map-builder')
-copy_files(release_path, 'script/tool')
-copy_files(release_path, 'script/ui-builder')
-copy_files(release_path, 'script/main.lua')
-copy_files(release_path, 'script/map.lua')
-copy_files(release_path, 'script/utility.lua')
-copy_files(release_path, 'template')
-copy_files(release_path, 'config.ini')
-copy_files(release_path, 'w3x2lni.exe')
+release_path = root / ('w3x2lni_v'..version)
+create_directory()
+copy_files('bin')
+copy_files('data')
+copy_files('plugin/本地脚本.lua')
+copy_files('plugin/模型加密.lua')
+copy_files('plugin/示例.lua')
+copy_files('plugin/资源保护.lua')
+copy_files('script/core')
+copy_files('script/ffi')
+copy_files('script/gui')
+copy_files('script/map-builder')
+copy_files('script/tool')
+copy_files('script/ui-builder')
+copy_files('script/main.lua')
+copy_files('script/map.lua')
+copy_files('script/utility.lua')
+copy_files('template')
+copy_files('config.ini')
+copy_files('w3x2lni.exe')
+
+copy_files('script/test')
+unit_test()
+remove_files('script/test')
 
 print('完成')
