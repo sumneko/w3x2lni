37f6dfc52564abf27efcc11be780a6f48147ce8d
最萌小汐 <sumneko@hotmail.com>
2018-04-02 10:59:10 +0800

    命令行模式

---
+++ 命令行-obj.bat
@@ -0,0 +1,12 @@
+@echo OFF
+if EXIST %1 goto convert
+echo Çë½«µØÍ¼ÍÏÈëbat
+pause
+goto finish
+
+:convert
+CD script
+.\..\bin\w2l-worker.exe -e "package.cpath = [[.\\..\\bin\\?.dll]]" .\gui\mini.lua -obj "%1" > ./../log.txt
+CD ..
+
+:finish
