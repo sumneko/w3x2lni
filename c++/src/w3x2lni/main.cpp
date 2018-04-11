63640edc070ff030cce8fa1f5110a62ebb0afdcd
actboy168 <actboy168@gmail.com>
2018-04-11 16:42:09 +0800

    捕捉错误

--- c++/src/w3x2lni/main.cpp
+++ c++/src/w3x2lni/main.cpp
@@ -1,6 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <Windows.h>
 #include <shlwapi.h>
+#include <algorithm>
+#include <utility>
+extern "C" {
+#include "../utf8/unicode.h"
+}
 #pragma comment(lib, "shlwapi.lib")
 
 struct strview {
@@ -64,12 +69,40 @@ struct path : public strbuilder<MAX_PATH> {
 	}
 };
 
+static std::pair<HANDLE, HANDLE> pipe_open(int type)
+{
+	SECURITY_ATTRIBUTES sa;
+	sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+	sa.bInheritHandle = FALSE;
+	sa.lpSecurityDescriptor = NULL;
+	HANDLE rd = NULL, wr = NULL;
+	if (!::CreatePipe(&rd, &wr, &sa, 0)) {
+		return std::make_pair((HANDLE)NULL, (HANDLE)NULL);
+	}
+	HANDLE f = type == 'r' ? rd : wr;
+	HANDLE h = type == 'r' ? wr : rd;
+	return std::make_pair(f, h);
+}
+
+void Error() 
+{
+	wchar_t* msg = 0;
+	DWORD ok = ::FormatMessageW(
+		FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL,
+		::GetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		reinterpret_cast<LPWSTR>(&msg),
+		0,
+		NULL);
+	if (ok && msg)
+	{
+		MessageBoxW(0, msg, L"Error!", 0);
+	}
+}
+
 int __stdcall wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
 {
-	STARTUPINFOW            si = { sizeof STARTUPINFOW };
-	PROCESS_INFORMATION     pi = { 0 };
-	DWORD flags = NORMAL_PRIORITY_CLASS | CREATE_UNICODE_ENVIRONMENT | CREATE_NO_WINDOW;
-
 	path app = path() / L"bin" / L"w2l-worker.exe";
 	path cwd = path() / L"script";
 	strbuilder<32768> cmd;
@@ -90,31 +123,63 @@ int __stdcall wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCm
 	env += path() / L"bin";
 	env += L"\0";
 
+	auto [err_f, err_h] = pipe_open('r');
+	if (!err_f || !err_h) {
+		Error();
+		return -1;
+	}
+	::SetHandleInformation(err_h, HANDLE_FLAG_INHERIT, HANDLE_FLAG_INHERIT);
+
+	STARTUPINFOW            si = { sizeof STARTUPINFOW };
+	PROCESS_INFORMATION     pi = { 0 };
+	si.dwFlags = STARTF_USESTDHANDLES;
+	si.hStdInput = INVALID_HANDLE_VALUE;
+	si.hStdOutput = INVALID_HANDLE_VALUE;
+	si.hStdError = err_h;
+
 	if (!::CreateProcessW(
 		app.string(),
 		cmd.string(),
-		NULL, NULL, FALSE, flags,
+		NULL, NULL, TRUE, 
+		NORMAL_PRIORITY_CLASS | CREATE_UNICODE_ENVIRONMENT | CREATE_NO_WINDOW,
 		env.string(),
 		cwd.string(),
-		&si, &pi))
+		&si, &pi)) 
 	{
-		wchar_t* msg = 0;
-		DWORD ok = ::FormatMessageW(
-			FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS,
-			NULL,
-			::GetLastError(),
-			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-			reinterpret_cast<LPWSTR>(&msg),
-			0,
-			NULL);
-		if (ok && msg)
-		{
-			MessageBoxW(0, msg, L"Error!", 0);
-		}
+		::CloseHandle(err_f);
+		::CloseHandle(err_h);
+		Error();
 		return -1;
 	}
 
+	::CloseHandle(err_h);
 	::CloseHandle(pi.hThread);
 	::CloseHandle(pi.hProcess);
+
+	char msg[2048];
+	size_t pos = 0;
+	for (;;) {
+		DWORD rlen = 0;
+		if (!PeekNamedPipe(err_f, 0, 0, 0, &rlen, 0)) {
+			break;
+		}
+		if (rlen == 0) {
+			Sleep(200);
+			continue;
+		}
+		DWORD len = (std::min)(rlen, (DWORD)(sizeof msg - pos - 1));
+		if (rlen == 0) {
+			break;
+		}
+		if (!ReadFile(err_f, msg + pos, len, &rlen, 0)) {
+			break;
+		}
+		pos += rlen;
+	}
+	if (pos) {
+		msg[pos] = 0;
+		MessageBoxW(0, u2w(msg), L"Error!", 0);
+	}
+	::CloseHandle(err_f);
 	return 0;
 }
