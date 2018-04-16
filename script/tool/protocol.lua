6fb220ef79359306486e19b79f8be5d12b153c59
actboy168 <actboy168@gmail.com>
2018-04-16 16:32:31 +0800

    前端支持新的后端协议

--- script/tool/protocol.lua
+++ script/tool/protocol.lua
@@ -3,29 +3,37 @@ local function send(type, args)
     io.stdout:write(('Content-Length:%d\r\n%s'):format(#content, content))
 end
 
+local function execute(s)
+    return assert(load('return (' .. s .. ')', '=(protocol)', 't', {}))()
+end
+
 local function recv(s, bytes)
-    s.bytes = s.bytes or s.bytes .. bytes and bytes
-    if s.length then
-        if s.length >= #s.bytes then
-            local res = s.bytes:sub(1, s.length)
-            s.bytes:sub(s.length + 1)
-            s.length = nil
-            return res
+    bytes = bytes or ''
+    s.bytes = s.bytes and (s.bytes .. bytes) or bytes
+    while true do
+        if s.length then
+            if s.length <= #s.bytes then
+                local res = s.bytes:sub(1, s.length)
+                s.bytes = s.bytes:sub(s.length + 1)
+                s.length = nil
+                return execute(res)
+            end
+            return
         end
-        return
+        local pos = s.bytes:find('\r\n', 1, true)
+        if not pos then
+            return
+        end
+        if pos <= 15 or s.bytes:sub(1, 15) ~= 'Content-Length:' then
+            return error('Invalid protocol.')
+        end
+        local length = tonumber(s.bytes:sub(16, pos))
+        if not length then
+            return error('Invalid protocol.')
+        end
+        s.bytes = s.bytes:sub(pos + 2)
+        s.length = length
     end
-    local pos = s.bytes:find('\r\n', 1, true)
-    if not pos then
-        return
-    end
-    if pos <= 15 or s.bytes:sub(1, 15) ~= 'Content-Length:' then
-        return error('Invalid protocol.')
-    end
-    local length = tonumber(s.bytes:sub(16, pos))
-    if not length then
-        return error('Invalid protocol.')
-    end
-    s.length = length + 2
 end
 
 return {
