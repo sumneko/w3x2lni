70390db76553f55d19bf5d1549056a9d4c0451d7
最萌小汐 <sumneko@hotmail.com>
2018-04-18 16:58:30 +0800

    根据语言和版本算出mpq目录名

--- script/tool/custom_mpq.lua
+++ script/tool/custom_mpq.lua
@@ -6,28 +6,29 @@ local makefile = require 'prebuilt.makefile'
 local config = require 'tool.config'
 local proto = require 'tool.protocol'
 local lang = require 'tool.lang'
+local file_version = require 'ffi.file_version'
 local w2l
 local mpq_name
 local root = fs.current_path()
 
 local language_map = {
-    ['0x00000409'] = 'enUS',
-    ['0x00000809'] = 'enGB',
-    ['0x0000040c'] = 'frFR',
-    ['0x00000407'] = 'deDE',
-    ['0x0000040a'] = 'esES',
-    ['0x00000410'] = 'itIT',
-    ['0x00000405'] = 'csCZ',
-    ['0x00000419'] = 'ruRU',
-    ['0x00000415'] = 'plPL',
-    ['0x00000416'] = 'ptBR',
-    ['0x00000816'] = 'ptPT',
-    ['0x0000041f'] = 'tkTK',
-    ['0x00000411'] = 'jaJA',
-    ['0x00000412'] = 'koKR',
-    ['0x00000404'] = 'zhTW',
-    ['0x00000804'] = 'zhCN',
-    ['0x0000041e'] = 'thTH',
+    [0x00000409] = 'enUS',
+    [0x00000809] = 'enGB',
+    [0x0000040c] = 'frFR',
+    [0x00000407] = 'deDE',
+    [0x0000040a] = 'esES',
+    [0x00000410] = 'itIT',
+    [0x00000405] = 'csCZ',
+    [0x00000419] = 'ruRU',
+    [0x00000415] = 'plPL',
+    [0x00000416] = 'ptBR',
+    [0x00000816] = 'ptPT',
+    [0x0000041f] = 'tkTK',
+    [0x00000411] = 'jaJA',
+    [0x00000412] = 'koKR',
+    [0x00000404] = 'zhTW',
+    [0x00000804] = 'zhCN',
+    [0x0000041e] = 'thTH',
 }
 
 local function mpq_language(mpq)
@@ -39,7 +40,23 @@ local function mpq_language(mpq)
     if not id then
         return nil
     end
-    return language_map[id]
+    return language_map[tonumber(id)]
+end
+
+local function war3_ver(input)
+    local exe_path = input / 'War3.exe'
+    if fs.exists(exe_path) then
+        local ver = file_version(exe_path:string())
+        if ver.major > 1 or ver.minor >= 29 then
+            return ('%d.%d.%d'):format(ver.major, ver.minor, ver.revision)
+        end
+    end
+    local dll_path = input / 'Game.dll'
+    if fs.exists(dll_path) then
+        local ver = file_version(dll_path:string())
+        return ('%d.%d.%d'):format(ver.major, ver.minor, ver.revision)
+    end
+    return nil
 end
 
 local function task(f, ...)
@@ -137,13 +154,21 @@ return function (_w2l, input)
         w2l.messager.text(lang.script.NEED_WAR3_DIR)
         return
     end
-
+    local ver = war3_ver(input)
+    if not ver then
+        w2l.messager.text(lang.script.NEED_WAR3_DIR)
+        return
+    end
     local mpqs = open_mpq(input)
     if not mpqs then
         return
     end
     local lg = mpq_language(mpqs['War3.mpq'])
-    mpq_name = lg or input:filename():string()
+    if lg then
+        mpq_name = ver .. '-' .. lg
+    else
+        mpq_name = input:filename():string()
+    end
     
     function w2l:mpq_load(filename)
         local mpq_path = root:parent_path() / 'data' / 'mpq'
