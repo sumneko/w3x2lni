d0739334c7fdba4be96d0eb8be8df63cf69f6dc7
最萌小汐 <sumneko@hotmail.com>
2018-04-19 10:52:09 +0800

    整理代码

---
+++ script/backend/unpack_config.lua
@@ -0,0 +1,32 @@
+local command = require 'backend.command'
+local lni = require 'lni'
+local root_path = require 'backend.root_path'
+require 'utility'
+
+return function ()
+    local config = {}
+    if command.config then
+        config.config_path = command.config
+    end
+    if command[2] then
+        config.input = root_path(command[2])
+    end
+    if command[3] then
+        config.output = root_path(command[3])
+    end
+
+    if not config.config_path then
+        config.config_path = 'config.ini'
+    end
+    local config_path = root_path(config.config_path)
+    local tbl = lni(io.load(config_path))
+    for k, v in pairs(tbl.global) do
+        config[k] = v
+    end
+    if type(tbl[config.mode]) == 'table' then
+        for k, v in pairs(tbl[config.mode]) do
+            config[k] = v
+        end
+    end
+    return config
+end
