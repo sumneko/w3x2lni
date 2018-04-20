a72db2c774731846ce500496550a22878b314884
actboy168 <actboy168@gmail.com>
2018-04-20 22:16:56 +0800

    配置支持新的底层实现

--- script/backend/cli_config.lua
+++ script/backend/cli_config.lua
@@ -2,20 +2,24 @@ local messager = require 'tool.messager'
 local config1, config2 = require 'tool.config' ()
 local lang = require 'tool.lang'
 
-local function show_config(map, section, k, v)
-    messager.raw(('%s %s.%s = %s\r\n'):format(map and '[map] ' or '      ', section, k, tostring(v)))
+local function show_config(section, k, v)
+    if v[3] ~= nil then
+        messager.raw(('%s %s.%s = %s\r\n'):format('[地图]  ', section, k, tostring(v[3])))
+        return
+    end
+    if v[2] ~= nil then
+        messager.raw(('%s %s.%s = %s\r\n'):format('        ', section, k, tostring(v[2])))
+        return
+    end
+    messager.raw(('%s %s.%s = %s\r\n'):format('[默认]  ', section, k, tostring(v[1])))
 end
 
 return function (command)
     if not command[2] then
         messager.raw(lang.raw.CONFIG_DISPLAY .. '\r\n\r\n')
         for section, tbl in pairs(config2) do
-            for k, data in pairs(tbl) do
-                if data[3] == nil then
-                    show_config(false, section, k, data[2])
-                else
-                    show_config(true, section, k, data[3])
-                end
+            for k, v in pairs(tbl) do
+                show_config(section, k, v)
             end
         end
         return
@@ -27,11 +31,7 @@ return function (command)
         messager.raw(lang.raw.CONFIG_DISPLAY .. '\r\n\r\n')
         local v = config2[section][k]
         if v then
-            if v[3] == nil then
-                show_config(true, section, k, v[2])
-            else
-                show_config(false, section, k, v[3])
-            end
+            show_config(section, k, v)
         else
             messager.raw(lang.raw.CONFIG_ERROR)
             messager.raw('\r\n')
@@ -47,12 +47,14 @@ return function (command)
         end
         config1[section][k] = v
         lang:set_lang(config1.global.lang)
+
         messager.raw(lang.raw.CONFIG_UPDATE .. '\r\n\r\n')
-        show_config(false, section, k, v)
+        show_config(section, k, {nil, v})
+
         if config2[section][k][3] ~= nil then
             messager.raw('\r\n')
             messager.raw('但实际生效的是地图配置:\r\n\r\n')
-            show_config(true, section, k, config2[section][k][3])
+            show_config(section, k, config2[section][k])
         end
         return
     end
