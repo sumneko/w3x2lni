f1f97d5bd76bac25ca408516ea9b372604e22544
最萌小汐 <sumneko@hotmail.com>
2018-04-27 17:59:16 +0800

    加个单元测试

---
+++ test/unit_test/misc数据的清理与保留/test.lua
@@ -0,0 +1,13 @@
+local w2l = w3x2lni()
+w2l:set_config { mode = 'slk', read_slk = true }
+w2l.slk = {}
+w2l:frontend()
+assert(w2l.slk.misc.HERO._source == 'slk', '来自slk的misc要有标记')
+w2l:backend()
+local ok = false
+for k in pairs(w2l.slk.misc.HERO) do
+    if k:sub(1, 1) ~= '_' then
+        ok = true
+    end
+end
+assert(ok, 'slk时来自slk的misc不应该清理数据')
