623a5bb06e1903dc171ff1ffd6b7073194fb8953
最萌小汐 <sumneko@hotmail.com>
2018-04-27 19:40:08 +0800

    改文本

--- script/core/slk/backend.lua
+++ script/core/slk/backend.lua
@@ -227,7 +227,9 @@ local function remove_unuse(w2l, slk)
         w2l.messager.report(lang.report.SIMPLIFY, 4, lang.report.SIMPLIFIED_OBJECT:format(unuse_origin + unuse_custom, total_origin + total_custom))
     end
     if total_origin - unuse_origin > 0 then
+        w2l.messager.report(lang.report.SIMPLIFY, 4, '---------------------------------------------------------------------')
         w2l.messager.report(lang.report.SIMPLIFY, 4, lang.report.RETAIN_DEFAULT_OBJECT:format(total_origin - unuse_origin, total_origin))
+        w2l.messager.report(lang.report.SIMPLIFY, 4, '---------------------------------------------------------------------')
         report_list(slk, origin_list, 'unit', 10)
         report_list(slk, origin_list, 'ability', 10)
         report_list(slk, origin_list, 'item', 10)
@@ -237,7 +239,9 @@ local function remove_unuse(w2l, slk)
         report_list(slk, origin_list, 'doodad', 3)
     end
     if unuse_custom > 0 then
+        w2l.messager.report(lang.report.SIMPLIFY, 4, '---------------------------------------------------------------------')
         w2l.messager.report(lang.report.SIMPLIFY, 4, lang.report.SIMPLIFIED_CUSTOM_OBJECT:format(unuse_custom, total_custom))
+        w2l.messager.report(lang.report.SIMPLIFY, 4, '---------------------------------------------------------------------')
         report_list(slk, custom_list, 'unit', 10)
         report_list(slk, custom_list, 'ability', 10)
         report_list(slk, custom_list, 'item', 10)
