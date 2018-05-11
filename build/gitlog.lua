ab994bf59c2865685c408f4aeed1935e7102285a
actboy168 <actboy168@gmail.com>
2018-05-11 22:21:46 +0800

    换个位置

--- build/gitlog.lua
+++ build/gitlog.lua
@@ -9,7 +9,7 @@ return function()
     local commit = lines[1]:match 'commit[ ]+([0-9|a-f]*)'
     local date = lines[4]:match 'Date:[ ]+([0-9|a-z|A-Z|%+|%:| ]*)'
 
-    local f = io.open((root / 'script' / 'gui' / 'gitlog.lua'):string(), 'w')
+    local f = io.open((root / 'script' / 'tool' / 'gitlog.lua'):string(), 'w')
     f:write(([[
 return {
     commit = '%s',
