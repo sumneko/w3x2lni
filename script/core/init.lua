a2d2c3c38e249ed1912699e21acc97c8f1fcfdba
最萌小汐 <sumneko@hotmail.com>
2018-05-11 11:31:10 +0800

    多余的代码

--- script/core/init.lua
+++ script/core/init.lua
@@ -222,45 +222,6 @@ function mt:__index(name)
     return nil
 end
 
-local function new_path()
-    local mt = {}
-    local paths = {'\\'}
-    local mpqs = {}
-    local function update()
-        paths = {'\\'}
-        for i = #mpqs, 1, -1 do
-            local path = mpqs[i]
-            local max = #paths
-            table.insert(paths, '\\' .. path .. '\\')
-            for i = 2, max do
-                table.insert(paths, '\\' .. path .. paths[i])
-            end
-        end
-    end
-    function mt:open(path)
-        table.insert(mpqs, path)
-        update()
-    end
-    function mt:close(path)
-        for i, mpq in ipairs(mpqs) do
-            if mpq == path then
-                table.remove(mpqs, i)
-                update()
-                return
-            end
-        end
-    end
-    function mt:each_path(callback)
-        for i = #paths, 1, -1 do
-            local res = callback(paths[i])
-            if res then
-                return res
-            end
-        end
-    end
-    return mt
-end
-
 function mt:map_load(filename)
     return nil
 end
