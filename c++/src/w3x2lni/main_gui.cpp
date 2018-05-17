36f07a870ce5a76fef2bcef940d0d572dcd71e64
actboy168 <actboy168@gmail.com>
2018-05-17 10:24:11 +0800

    删除没用的代码

--- c++/src/w3x2lni/main_gui.cpp
+++ c++/src/w3x2lni/main_gui.cpp
@@ -3,23 +3,6 @@ extern "C" {
 #include "../utf8/unicode.h"
 }
 
-void Error() 
-{
-	wchar_t* msg = 0;
-	DWORD ok = ::FormatMessageW(
-		FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL,
-		::GetLastError(),
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		reinterpret_cast<LPWSTR>(&msg),
-		0,
-		NULL);
-	if (ok && msg)
-	{
-		MessageBoxW(0, msg, L"Error!", 0);
-	}
-}
-
 int __stdcall wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
 {
 	pipe err;
