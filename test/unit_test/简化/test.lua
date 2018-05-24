bb048eb82bbb151edbd24cfc64206e16d0f91f1c
最萌小汐 <sumneko@hotmail.com>
2018-05-24 10:30:02 +0800

    简化的单元测试

---
+++ test/unit_test/简化/test.lua
@@ -0,0 +1,21 @@
+local w2l = w3x2lni()
+
+w2l:set_setting
+{
+    mode = 'slk',
+}
+
+function w2l:call_plugin(name)
+    if name == 'on_mark' then
+        return { H000 = true }
+    end
+end
+
+function w2l.input_ar:get(name)
+    return read(name)
+end
+
+w2l:frontend()
+w2l:backend()
+
+assert(w2l.slk.ability.A000._mark)
