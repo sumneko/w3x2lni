819fef5b0a12f939de408cfc5d5f23a2e0bcb017
最萌小汐 <sumneko@hotmail.com>
2018-07-07 21:47:17 +0800

    txt类型漏了

--- script/core/slk/frontend_slk.lua
+++ script/core/slk/frontend_slk.lua
@@ -117,12 +117,12 @@ local function txt_to_type(tp, value)
         if not value then
             return 0
         end
-        return math_floor(wtonumber(value))
+        return math_floor(wtonumber(value)) -- txt中的整数支持256进制、16进制和8进制表达方式，因此要使用wtonumber
     elseif tp == 1 or tp == 2 then
         if not value then
-            return 0.0
+            return 0
         end
-        return wtonumber(value) + 0.0
+        return value
     elseif tp == 3 then
         if not value then
             return nil
