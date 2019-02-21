ef656946c54d0ec71ee1c350ef538af05dcd3756
最萌小汐 <sumneko@hotmail.com>
2019-02-21 10:22:32 +0800

    slk数据类型的测试

---
+++ test/unit_test/数据解析类型/test.lua
@@ -0,0 +1,7 @@
+local w2l = w3x2lni()
+
+local slk = w2l:parse_slk(w2l:mpq_load 'units\\abilitydata.slk')
+
+assert(slk.ANcl.DataD1 == '0.98')
+assert(slk.ANcl.DataE1 == '1')
+assert(slk.ANcl.DataF1 == 'channel')
