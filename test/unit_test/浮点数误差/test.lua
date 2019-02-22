fa06ea0086b9ed0297a5fa5a5f3fb967ae68e9c6
最萌小汐 <sumneko@hotmail.com>
2019-02-22 11:19:49 +0800

    测试

--- test/unit_test/浮点数误差/test.lua
+++ test/unit_test/浮点数误差/test.lua
@@ -3,11 +3,15 @@ local w2l = w3x2lni()
 w2l:set_setting
 {
     mode = 'slk',
+    read_slk = true,
 }
 
 function w2l:call_plugin(name)
     if name == 'on_mark' then
-        return { A000 = true }
+        return {
+            A000 = true,
+            A001 = true,
+        }
     end
 end
 
@@ -19,3 +23,4 @@ w2l:frontend()
 w2l:backend()
 
 assert(w2l.slk.ability.A000.ubertip[1] == '90%')
+assert(w2l.slk.ability.A001.dur[1] == 1.234567890123456)
