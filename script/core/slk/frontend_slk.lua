c36e5acd263fdc29d87b6fe05504a8ee7591f2ae
最萌小汐 <sumneko@hotmail.com>
2019-02-22 10:42:11 +0800

    slk解析实数为number

--- script/core/slk/frontend_slk.lua
+++ script/core/slk/frontend_slk.lua
@@ -23,13 +23,9 @@ local function slk_to_type(tp, value)
         return math_floor(wtonumber(value))
     elseif tp == 1 or tp == 2 then
         if not value then
-            return '0'
+            return 0.0
         end
-        value = value:gsub("^%s*(.-)%s*$", "%1")
-        if value == '-' then
-            return '0'
-        end
-        return value
+        return wtonumber(value) + 0.0
     elseif tp == 3 then
         if not value then
             return ''
--- test/unit_test/数据解析类型/test.lua
+++ test/unit_test/数据解析类型/test.lua
@@ -2,6 +2,6 @@ local w2l = w3x2lni()
 
 local slk = w2l:parse_slk(w2l:mpq_load 'units\\abilitydata.slk')
 
-assert(slk.ANcl.DataD1 == '0.98')
-assert(slk.ANcl.DataE1 == '1')
+assert(slk.ANcl.DataD1 == 0.98)
+assert(slk.ANcl.DataE1 == 1)
 assert(slk.ANcl.DataF1 == 'channel')
