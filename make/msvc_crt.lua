bf90fdfa3654ea9a1ec9f1e36c8d08a456c112fe
actboy168 <actboy168@gmail.com>
2019-02-28 10:35:22 +0800

    漏了一个dll

--- make/msvc_crt.lua
+++ make/msvc_crt.lua
@@ -49,6 +49,7 @@ end
 return function (platform, target)
     fs.create_directories(target)
     fs.copy_file(crtpath(platform) / 'msvcp140.dll', target / 'msvcp140.dll', true)
+    fs.copy_file(crtpath(platform) / 'concrt140.dll', target / 'concrt140.dll', true)
     fs.copy_file(crtpath(platform) / 'vcruntime140.dll', target / 'vcruntime140.dll', true)
     for dll in ucrtpath(platform):list_directory() do
         fs.copy_file(dll, target / dll:filename(), true)
