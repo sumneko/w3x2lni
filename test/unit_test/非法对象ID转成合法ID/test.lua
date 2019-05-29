04cf77f9db97e6137d764fe91cfd9163a09214df
sumneko <sumneko@hotmail.com>
2019-05-29 14:16:26 +0800

    Revert "slk时带有非法字符的ID的对象一律保留在obj中"
    This reverts commit 5a7fa263ee7fe4b1fddac6962f663ace3e2ce198.

--- test/unit_test/非法对象ID转成合法ID/test.lua
+++ test/unit_test/非法对象ID转成合法ID/test.lua
@@ -12,9 +12,9 @@ local backend_slk = w2l.backend_slk
 function w2l:backend_obj(type, ...)
     local buf = backend_obj(self, type, ...)
     if type == 'ability' then
-        assert(buf:find('AHtb-000', 1, true))
-        assert(buf:find('AHtb.AAA', 1, true))
-        assert(buf:find('AHtbAAA]', 1, true))
+        assert(buf:find('!000-000', 1, true))
+        assert(buf:find('!001.AAA', 1, true))
+        assert(buf:find('!002AAA]', 1, true))
         ok1 = buf
     end
     return buf
@@ -24,9 +24,11 @@ function w2l:backend_slk(type, slk_name, ...)
     local buf = backend_slk(self, type, slk_name, ...)
     if slk_name == 'units\\abilitydata.slk' then
         assert(not buf:find('-000', 1, true))
+        assert(buf:find('!000', 1, true))
         assert(not buf:find('.AAA', 1, true))
+        assert(buf:find('!001', 1, true))
         assert(not buf:find('AAA]', 1, true))
-        assert(buf:find('AHtb', 1, true))
+        assert(buf:find('!002', 1, true))
         ok2 = true
     end
     return buf
@@ -45,4 +47,3 @@ local objs = w2l:frontend_obj('ability', ok1)
 assert(objs['-000'].anam[1] == slk.ability['-000'].name)
 assert(objs['.AAA'].anam[1] == slk.ability['.AAA'].name)
 assert(objs['AAA]'].anam[1] == slk.ability['AAA]'].name)
-assert(slk.ability['AHtb']._mark == true)
