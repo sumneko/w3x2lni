4b374be744a73a15eca61734ac4c345564adaca9
最萌小汐 <sumneko@hotmail.com>
2019-11-13 13:31:58 +0800

    casclib

---
+++ script/ffi/casclib.lua
@@ -0,0 +1,126 @@
+local ffi = require 'ffi'
+local loaddll = require 'ffi.loaddll'
+
+ffi.cdef[[
+    bool CascOpenStorage(const wchar_t* szParams, unsigned long dwLocaleMask, uint32_t* phStorage);
+    bool CascCloseStorage(uint32_t* hStorage);
+    bool CascOpenFile(uint32_t hStorage, const char* szFileName, unsigned long dwLocaleFlags, unsigned long dwOpenFlags, uint32_t* PtrFileHandle);
+    unsigned long CascGetFileSize(uint32_t hFile, unsigned long* pdwFileSizeHigh);
+    bool CascReadFile(uint32_t hFile, void* lpBuffer, unsigned long dwToRead, unsigned long* pdwRead);
+    bool CascCloseFile(uint32_t hFile);
+]]
+
+loaddll 'casclib'
+fs = require 'bee.filesystem'
+local uni = require 'ffi.unicode'
+local casclib = ffi.load('casclib')
+
+local rfile = {}
+rfile.__index = rfile
+
+function rfile:size()
+    if self.handle == 0 then
+        return 0
+    end
+    local size_hi = ffi.new('unsigned long[1]', 0)
+    local size_lo = casclib.CascGetFileSize(self.handle, size_hi)
+    return size_lo | (size_hi[0] << 32)
+end
+
+function rfile:read(n)
+    if self.handle == 0 then
+        return
+    end
+    if not n then
+        n = self:size()
+    end
+    local buf = ffi.new('char[?]', n)
+    local pread = ffi.new('unsigned long[1]', 0)
+    if not casclib.CascReadFile(self.handle, buf, n, pread) then
+        return nil
+    end
+    return ffi.string(buf, pread[0])
+end
+
+function rfile:close()
+    if self.handle == 0 then
+        return
+    end
+    casclib.CascCloseFile(self.handle)
+    self.handle = 0
+end
+
+rfile.__close = rfile.close
+
+local archive = {}
+archive.__index = archive
+
+function archive:close()
+    if self.handle == 0 then
+        return
+    end
+    casclib.CascCloseFile(self.handle)
+    self.handle = 0
+end
+
+function archive:extract(name, path)
+    if self.handle == 0 then
+        return false
+    end
+    local file <close> = self:open_file(name)
+    if not file then
+        return false
+    end
+    local dir = path:parent_path()
+    if not fs.exists(dir) then
+        fs.create_directories(dir)
+    end
+    local content = file:read()
+    if not content then
+        return false
+    end
+    local f <close> = io.open(path:string(), 'wb')
+    if not f then
+        return false
+    end
+    f:write(content)
+    return true
+end
+
+function archive:open_file(name)
+    if self.handle == 0 then
+        return nil
+    end
+    local phandle = ffi.new('uint32_t[1]', 0)
+    if not casclib.CascOpenFile(self.handle, name, 0, 0, phandle) then
+        return nil
+    end
+    return setmetatable({ handle = phandle[0] }, rfile)
+end
+
+function archive:load_file(name)
+    if self.handle == 0 then
+        return nil
+    end
+    local file = self:open_file(name)
+    if not file then
+        return nil
+    end
+    local content = file:read()
+    file:close()
+    return content
+end
+
+archive.__close = archive.close
+
+local m = {}
+function m.open(path)
+    local wpath = uni.u2w(path)
+    local phStorage = ffi.new('uint32_t[1]', 0)
+    if not casclib.CascOpenStorage(wpath, 0, phStorage) then
+        return nil
+    end
+    return setmetatable({ handle = phStorage[0] }, archive)
+end
+
+return m
--- script/share/war3.lua
+++ script/share/war3.lua
@@ -1,5 +1,6 @@
 local file_version = require 'ffi.file_version'
 local stormlib = require 'ffi.stormlib'
+local casclib = require 'ffi.casclib'
 
 local language_map = {
     [0x00000409] = 'enUS',
@@ -43,20 +44,27 @@ local function war3_ver (input)
     if fs.exists(exe_path) then
         local ver = file_version(exe_path:string())
         if ver.major > 1 or ver.minor >= 29 then
-            return ('%d.%d.%d'):format(ver.major, ver.minor, ver.revision)
+            return ('%d.%d.%d'):format(ver.major, ver.minor, ver.revision), ver
         end
     end
     local exe_path = input / 'Warcraft III.exe'
     if fs.exists(exe_path) then
         local ver = file_version(exe_path:string())
         if ver.major > 1 or ver.minor >= 29 then
-            return ('%d.%d.%d'):format(ver.major, ver.minor, ver.revision)
+            return ('%d.%d.%d'):format(ver.major, ver.minor, ver.revision), ver
+        end
+    end
+    local exe_path = input / 'x86_64' / 'Warcraft III.exe'
+    if fs.exists(exe_path) then
+        local ver = file_version(exe_path:string())
+        if ver.major > 1 or ver.minor >= 29 then
+            return ('%d.%d.%d'):format(ver.major, ver.minor, ver.revision), ver
         end
     end
     local dll_path = input / 'Game.dll'
     if fs.exists(dll_path) then
         local ver = file_version(dll_path:string())
-        return ('%d.%d.%d'):format(ver.major, ver.minor, ver.revision)
+        return ('%d.%d.%d'):format(ver.major, ver.minor, ver.revision), ver
     end
     return nil
 end
@@ -64,23 +72,29 @@ end
 local m = {}
 
 function m:open(path)
-    local war3_ver = war3_ver(path)
-    if not war3_ver then
+    local verStr, ver = war3_ver(path)
+    if not verStr then
         return false
     end
-    self.mpqs = {}
-    for _, mpqname in ipairs {
-        'War3Patch.mpq',
-        'War3xLocal.mpq',
-        'War3x.mpq',
-        'War3Local.mpq',
-        'War3.mpq',
-    } do
-        self.mpqs[#self.mpqs+1] = stormlib.open(path / mpqname, true)
-    end
-    local lg = mpq_language(self:readfile('config.txt'))
-    if lg then
-        self.name = lg .. '-' .. war3_ver
+    if ver.major > 1 or ver.minor >= 29 then
+        self.casc = casclib.open(path:string())
+        local lg = require 'ffi.language' ()
+        self.name = lg .. '-' .. verStr
+    else
+        self.mpqs = {}
+        for _, mpqname in ipairs {
+            'War3Patch.mpq',
+            'War3xLocal.mpq',
+            'War3x.mpq',
+            'War3Local.mpq',
+            'War3.mpq',
+        } do
+            self.mpqs[#self.mpqs+1] = stormlib.open(path / mpqname, true)
+        end
+        local lg = mpq_language(self:readfile('config.txt'))
+        if lg then
+            self.name = lg .. '-' .. verStr
+        end
     end
     return true
 end
@@ -101,10 +115,14 @@ function m:readfile(filename)
 end
 
 function m:extractfile(filename, targetpath)
-    for _, mpq in ipairs(self.mpqs) do
-        if mpq:has_file(filename) then
-            return mpq:extract(filename, targetpath)
+    if self.mpqs then
+        for _, mpq in ipairs(self.mpqs) do
+            if mpq:has_file(filename) then
+                return mpq:extract(filename, targetpath)
+            end
         end
+    elseif self.casc then
+        self.casc:extract('war3.w3mod:'..filename, targetpath)
     end
 end
 
