ab495e5b5345a564fe095592790adf3f2d4d60ff
最萌小汐 <sumneko@hotmail.com>
2019-11-26 17:02:02 +0800

    测试一下是否是因为没有读到文件

--- test/unit_test/wtg转lml-新版本/test.lua
+++ test/unit_test/wtg转lml-新版本/test.lua
@@ -60,10 +60,12 @@ assert(equal(files2, targetfiles2))
 
 
 local w2l = w3x2lni()
-w2l:set_setting { mode = 'obj',  data_ui = '${DATA}' }
+w2l:set_setting { mode = 'obj', data_ui = '${DATA}' }
 
 local wtg_data, wct_data = w2l:frontend_lml(function (filename)
-    return read('trigger/' .. filename)
+    local buf = read('trigger/' .. filename)
+    print('read', filename, buf and #buf or buf)
+    return buf
 end)
 local targetwtg = w2l:backend_wtg(wtg_data)
 local targetwct = w2l:backend_wct(wct_data)
