4f511ab0d1b80c920083673cd29a06e24d00a8c5
最萌小汐 <sumneko@hotmail.com>
2020-04-17 13:58:10 +0800

    上限增加到一万级

--- script/core/slk/frontend_merge.lua
+++ script/core/slk/frontend_merge.lua
@@ -85,8 +85,8 @@ end
 local function copy_obj(a, b)
     local c = {}
     local lv = tonumber(b._max_level or a._max_level)
-    if lv and lv > 100 then
-        lv = 100
+    if lv and lv > 10000 then
+        lv = 10000
     end
     if b._code and a._code ~= b._code then
         w2l.messager.report(lang.report.INVALID_OBJECT_DATA, 6, lang.report.ABILITY_CODE_ERROR:format(b._id, b._code), lang.report.DEFAULT_IS:format(a._code))
@@ -118,8 +118,8 @@ end
 local function fill_obj(a)
     local c = {}
     local lv = a._max_level
-    if lv and lv > 100 then
-        lv = 100
+    if lv and lv > 10000 then
+        lv = 10000
     end
     for k, v in pairs(a) do
         if type(v) == 'table' then
