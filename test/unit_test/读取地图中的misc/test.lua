a6e3199f71b51738b62a1fcfcdd83c0fc10c90fa
最萌小汐 <sumneko@hotmail.com>
2022-05-28 02:31:41 +0800

    为啥ci过不了

--- test/unit_test/读取地图中的misc/test.lua
+++ test/unit_test/读取地图中的misc/test.lua
@@ -14,4 +14,4 @@ w2l:backend()
 
 assert(w2l.slk.misc.Misc.agiattackspeedbonus == '0.0123456789   ')
 assert(w2l.slk.misc.HERO.name == '两个英雄')
-assert(w2l.slk.misc.FontHeights.tooltipname == '0.015')
+assert(w2l.slk.misc.FontHeights.tooltipname == '0.015', w2l.slk.misc.FontHeights.tooltipname)
