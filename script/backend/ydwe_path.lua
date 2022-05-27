e6515dda9f2dc98277030c3d0e81aac18a95715d
最萌小汐 <sumneko@hotmail.com>
2022-05-28 03:10:31 +0800

    换个读注册表的方法

--- script/backend/ydwe_path.lua
+++ script/backend/ydwe_path.lua
@@ -1,15 +1,18 @@
 return function()
     local fs = require 'bee.filesystem'
-    local registry = require 'bee.registry'
-    local commands = registry.open [[HKEY_CURRENT_USER\SOFTWARE\Classes\YDWEMap\shell\run_war3\command]]
-    if not commands then
+    local f = io.popen([[reg query "HKEY_CURRENT_USER\SOFTWARE\Classes\YDWEMap\shell\run_war3\command"]], 'r')
+    if not f then
         return
     end
-    local command = commands['']
+    local command = f:read 'a'
+    f:close()
     if not command then
         return
     end
     local path = command:match '^"([^"]*)"'
+    if not path then
+        return
+    end
     local ydwe = fs.path(path):parent_path()
     if fs.exists(ydwe / 'YDWE.exe') then
         return ydwe
